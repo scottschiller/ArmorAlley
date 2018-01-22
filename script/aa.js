scottschiller/ArmorAlley

commit 26726832bc50d50e51dfd5ba0d9ec6b0375bc76b

Conditionally include `#moble-controls` when updating inventory / control UI.

script/aa.js
@@ -3002,7 +3002,13 @@
       // based on funds, update "affordability" bits of UI.
       var playerFunds = game.objects.endBunkers[0].data.funds;
 
-      var o = document.getElementById('player-status-bar');
+      var nodes = [
+        document.getElementById('player-status-bar');
+      ];
+
+      if (isMobile) {
+        nodes.push(document.getElementById('mobile-controls'));
+      }
 
       var toAdd = [];
       var toRemove = [];
@@ -3020,13 +3026,18 @@
 
       var i, j;
 
-      // add/remove expect space-delimited strings.
-      for (i = 0, j = toAdd.length; i < j; i++) {
-        utils.css.add(o, toAdd[i]);
-      }
-      for (i = 0, j = toRemove.length; i < j; i++) {
-        utils.css.remove(o, toRemove[i]);
-      }
+      nodes.forEach(function(o) {
+
+        // add/remove expect space-delimited strings.
+        for (i = 0, j = toAdd.length; i < j; i++) {
+          utils.css.add(o, toAdd[i]);
+        }
+
+        for (i = 0, j = toRemove.length; i < j; i++) {
+          utils.css.remove(o, toRemove[i]);
+        }
+
+      });
 
     }
 

commit a35446006bc8a0c700259d0d3a6939e30966588d

Derrr, fix `;` syntax error introduced in 26726832bc50d50e51dfd5ba0d9ec6b0375bc76b.

script/aa.js
@@ -3003,7 +3003,7 @@
       var playerFunds = game.objects.endBunkers[0].data.funds;
 
       var nodes = [
-        document.getElementById('player-status-bar');
+        document.getElementById('player-status-bar')
       ];
 
       if (isMobile) {