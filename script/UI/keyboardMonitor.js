09ab82335bdad18eb2e9cc9189095416955981ca
Scott Schiller <spam@schillmania.com>
2022-01-10 22:12:22 -0800

    `keyboardMonitor` to module.

---
+++ script/UI/keyboardMonitor.js
@@ -0,0 +1,308 @@
+import { game } from '../aa.js';
+import { utils } from '../core/utils.js';
+import { TYPES } from '../core/global.js';
+
+import {
+  setMissileMode,
+  bananaMode,
+  rubberChickenMode,
+  defaultMissileMode,
+  stopEvent
+} from '../aa.js';
+
+// recycled from survivor.js
+function KeyboardMonitor() {
+
+  let keys;
+  let events;
+
+  // hash for keys being pressed
+  const downKeys = {};
+
+  // meaningful labels for key values
+  const keyMap = {
+    banana: 66,
+    rubber_chicken: 67,
+    shift: 16,
+    ctrl: 17,
+    space: 32,
+    left: 37,
+    up: 38,
+    right: 39,
+    down: 40,
+    missileLauncher: 77,
+    tank: 84,
+    van: 86,
+    infantry: 73,
+    engineer: 69
+  };
+
+  events = {
+
+    keydown(e) {
+
+      if (game.data.paused) return;
+
+      // console.log(e.keyCode);
+
+      if (!e.metaKey && keys[e.keyCode]?.down) {
+        if (!downKeys[e.keyCode]) {
+          downKeys[e.keyCode] = true;
+          keys[e.keyCode].down(e);
+        }
+        if (keys[e.keyCode].allowEvent === undefined) {
+          return stopEvent(e);
+        }
+      }
+
+      return true;
+
+    },
+
+    keyup(e) {
+
+      if (game.data.paused) return;
+
+      if (!e.metaKey && downKeys[e.keyCode] && keys[e.keyCode]) {
+        downKeys[e.keyCode] = null;
+        if (keys[e.keyCode].up) {
+          keys[e.keyCode].up(e);
+        }
+        if (keys[e.keyCode].allowEvent === undefined) {
+          return stopEvent(e);
+        }
+      }
+
+      return true;
+
+    }
+
+  };
+
+  keys = {
+
+    // NOTE: Each function gets an (e) event argument.
+
+    // shift
+    13: {
+
+      allowEvent: true, // don't use stopEvent()
+
+      down() {
+
+        game.objects.helicopters[0].eject();
+
+      }
+
+    },
+
+    // shift
+    16: {
+
+      allowEvent: true, // don't use stopEvent()
+
+      down() {
+
+        game.objects.helicopters[0].setFiring(true);
+
+      }
+
+    },
+
+    // ctrl
+    17: {
+
+      allowEvent: true, // don't use stopEvent()
+
+      down() {
+
+        game.objects.helicopters[0].setBombing(true);
+
+      }
+
+    },
+
+    // space bar
+    32: {
+
+      down() {
+
+        game.objects.helicopters[0].setParachuting(true);
+
+      },
+
+      up() {
+
+        game.objects.helicopters[0].setParachuting(false);
+
+      }
+
+    },
+
+    // "m"
+    77: {
+
+      down() {
+
+        game.objects.inventory.order(TYPES.missileLauncherCamel);
+
+      }
+
+    },
+
+    // "t"
+    84: {
+
+      down() {
+
+        game.objects.inventory.order(TYPES.tank);
+
+      }
+
+    },
+
+    // "v"
+    86: {
+
+      down() {
+
+        game.objects.inventory.order(TYPES.van);
+
+      }
+
+    },
+
+    // "b" (banana)
+    66: {
+      down() {
+
+        // heat-seeking banana
+        setMissileMode(bananaMode);
+
+        game.objects.helicopters[0].setMissileLaunching(true);
+
+      },
+
+      up() {
+
+        game.objects.helicopters[0].setMissileLaunching(false);
+
+      }
+
+    },
+
+    // "c" (rubber chicken)
+    67: {
+
+      down() {
+
+        // heat-seeking rubber chicken
+        setMissileMode(rubberChickenMode);
+
+        game.objects.helicopters[0].setMissileLaunching(true);
+
+      },
+
+      up() {
+
+        game.objects.helicopters[0].setMissileLaunching(false);
+
+      }
+
+    },
+
+    // "x"
+    88: {
+
+      down() {
+
+        // standard heat-seeking missile
+        setMissileMode(defaultMissileMode);
+
+        game.objects.helicopters[0].setMissileLaunching(true);
+
+      },
+
+      up() {
+
+        game.objects.helicopters[0].setMissileLaunching(false);
+
+      }
+
+    },
+
+    // "e"
+    69: {
+
+      down() {
+
+        game.objects.inventory.order(TYPES.engineer);
+
+      }
+
+    },
+
+    // "i"
+    73: {
+
+      down() {
+
+        game.objects.inventory.order(TYPES.infantry);
+
+      }
+
+    }
+
+  };
+
+  function isDown(labelOrCode) {
+
+    // check for a pressed key based on '37' or 'left', etc.
+    return (keyMap[labelOrCode] !== undefined ? downKeys[keyMap[labelOrCode]] : downKeys[labelOrCode]);
+
+  }
+
+  function releaseAll() {
+
+    // reset all pressed key states.
+    let item;
+    for (item in downKeys) {
+      if (downKeys[item]) {
+        // simulate the keyup event
+        events.keyup({
+          keyCode: item
+        });
+      }
+    }
+
+  }
+
+  function attachEvents() {
+
+    utils.events.add(document, 'keydown', events.keydown);
+    utils.events.add(document, 'keyup', events.keyup);
+
+  }
+
+  // init?
+
+  function initKeyboardMonitor() {
+
+    attachEvents();
+
+  }
+
+  return {
+
+    init: initKeyboardMonitor,
+    isDown,
+    keydown: events.keydown,
+    keyMap,
+    keyup: events.keyup,
+    releaseAll
+
+  };
+
+}
+
+export { KeyboardMonitor };
