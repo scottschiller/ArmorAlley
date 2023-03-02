cf7ba25b311bb37b1e303bdb1ee08fe36fd05186
Scott Schiller <spam@schillmania.com>
2023-03-02 09:28:24 -0800

    Inventory refactor: Update types / constructors. Delayed init. Bug fix for overlapping units, no more hackish frame delays between creating objects.

--- script/UI/Inventory.js
+++ script/UI/Inventory.js
@@ -3,53 +3,14 @@ import { utils } from '../core/utils.js';
 import { TYPES } from '../core/global.js';
 import { playSound, sounds } from '../core/sound.js';
 import { common } from '../core/common.js';
-import { isGameOver } from '../core/logic.js';
-import { MissileLauncher } from '../units/MissileLauncher.js';
-import { Tank } from '../units/Tank.js';
-import { Van } from '../units/Van.js';
-import { Infantry } from '../units/Infantry.js';
-import { Engineer } from '../units/Engineer.js';
+import { collisionCheck, isGameOver } from '../core/logic.js';
+import { sprites } from '../core/sprites.js';
 
 const Inventory = () => {
 
   let css, data, dom, objects, orderNotificationOptions, exports;
 
-  function createObject(typeData, options = {}) {
-
-    // create and append a new (something) to its appropriate array.
-
-    let orderObject;
-
-    orderObject = typeData[1](options);
-
-    // ignore if this is the stub object case
-    if (!options.noInit) {
-
-      typeData[0].push(orderObject);
-
-      // force-append this thing, if it's on-screen right now
-      common.updateIsOnScreen(orderObject);
-
-      // set up the initial transition
-      utils.css.add(orderObject.dom.o, css.building);
-
-      // and start the "rise" animation
-      window.requestAnimationFrame(() => {
-        utils.css.add(orderObject.dom.o, css.ordering);
-
-        common.setFrameTimeout(() => {
-          if (!orderObject.dom?.o) return;
-          utils.css.remove(orderObject.dom.o, css.ordering);
-          utils.css.remove(orderObject.dom.o, css.building);
-        }, 2200);
-
-      });
-
-    }
-
-    return orderObject;
-
-  }
+  const STD_LOOK_AHEAD = 8;
 
   function processNextOrder() {
 
@@ -63,7 +24,7 @@ const Inventory = () => {
 
     }
 
-    if (data.queue.length) {
+    if (data.queue.length || data.waiting) {
 
       // first or subsequent queue items being built.
 
@@ -71,7 +32,29 @@ const Inventory = () => {
       data.frameCount = 0;
 
       // take the first item out of the queue for building.
-      objects.order = data.queue.shift();
+      if (!objects.order) {
+        // don't wait for the the first
+        objects.order = data.queue.shift();
+      } else if (!data.waiting) {
+        objects.order = data.queue.shift();
+        // wait if there are more in the queue
+        data.waiting = true;
+      }
+
+      if (data.waiting && objects.lastObject) {
+
+        // is there physical room for the next one?
+        const nextOrder = objects.order;
+
+        // collision check: if this thing would overlap, don't add it just yet
+          if (collisionCheck(nextOrder.data, objects.lastObject.data, STD_LOOK_AHEAD)) {
+          return;
+        } else {
+          data.waiting = false;
+        }
+  
+      }
+  
 
     } else if (data.building && !data.queue.length) {
 
@@ -86,7 +69,7 @@ const Inventory = () => {
         }
 
         // clear the queue that just finished.
-        game.objects.helicopters[0].updateInventoryQueue();
+        game.objects[TYPES.helicopter][0].updateInventoryQueue();
 
         // clear the copy of the queue used for notifications.
         data.queueCopy = [];
@@ -99,9 +82,19 @@ const Inventory = () => {
 
   }
 
-  function order(type, options) {
+  function isOrderingTV(queue = data.queueCopy) {
 
-    let typeData, orderObject, orderSize, cost, pendingNotification;
+    if (queue?.length !== 2) return;
+
+    return (queue[0].data.type === TYPES.tank && queue[1].data.type === TYPES.van);
+
+  }
+
+  function order(type, options = {}) {
+
+    // this should be called only by the human player, not the CPU
+
+    let orderObject, orderSize, cost, pendingNotification;
 
     if (isGameOver()) return;
 
@@ -111,8 +104,6 @@ const Inventory = () => {
     options.x = -72;
 
     // let's build something - provided you're good for the $$$, that is.
-
-    typeData = data.types[type];
 
     // infantry or engineer? handle those specially.
 
@@ -127,33 +118,28 @@ const Inventory = () => {
     }
 
     // Hack: make a temporary object, so we can get the relevant data for the actual order.
-    if (!options.isEnemy) {
-      options.noInit = true;
-    }
+    if (!options.isEnemy) options.noInit = true;
 
-    orderObject = createObject(typeData, options);
+    orderObject = game.addObject(type, options);
 
     // do we have enough funds for this?
     cost = orderObject.data.inventory.cost;
 
-    if (game.objects.endBunkers[0].data.funds >= cost) {
+    if (game.objects[TYPES.endBunker][0].data.funds >= cost) {
 
-      game.objects.endBunkers[0].data.funds -= cost;
+      game.objects[TYPES.endBunker][0].data.funds -= cost;
 
       game.objects.view.updateFundsUI();
 
-      if (!data.isEnemy) {
-
-        // hackish: this will be executed below.
-        pendingNotification = () => {
-          game.objects.notifications.add('Order: %s 🛠️', orderNotificationOptions);
-        }
-
-        // player may now be able to order things.
-        game.objects.helicopters[0].updateStatusUI({ funds: true });
+      // hackish: this will be executed below.
+      pendingNotification = () => {
+        game.objects.notifications.add(`Order: %s ${isOrderingTV() ? '📺' : '🛠️'}`, orderNotificationOptions);
       }
 
-    } else if (!data.isEnemy) {
+      // player may now be able to order things.
+      game.objects[TYPES.helicopter][0].updateStatusUI({ funds: true });
+
+    } else {
 
       // Insufficient funds. "We require more vespene gas."
       if (sounds.inventory.denied) {
@@ -168,7 +154,7 @@ const Inventory = () => {
           // long vs. short-hand copy, flag set once NSF is hit and the order completes
           const result = input.replace('%s1', data.canShowNSF ? '' : 'Insufficient funds: ')
             .replace('%s2', (data.canShowNSF ? '🚫 ' : '') + text.charAt(0).toUpperCase() + (data.canShowNSF ? '' : text.slice(1)))
-            .replace('%c1', game.objects.endBunkers[0].data.funds)
+            .replace('%c1', game.objects[TYPES.endBunker][0].data.funds)
             .replace('%c2', cost);
           return result;
         },
@@ -190,12 +176,13 @@ const Inventory = () => {
       data: orderObject.data,
       // how long to wait after last item before "complete" (for buffering space)
       completeDelay: orderObject.data.inventory.orderCompleteDelay || 0,
-      typeData,
+      type,
       options,
       size: orderSize,
       originalSize: orderSize,
       onOrderStart: null,
       onOrderComplete: null,
+      waiting: false
     };
 
     let queueEvents;
@@ -206,27 +193,24 @@ const Inventory = () => {
     // live `data.queue` is modified via `shift()` as it's processed.
     data.queueCopy.push(newOrder);
 
-    if (!newOrder.options.isEnemy) {
 
-      // update the UI
-      utils.css.add(dom.gameStatusBar, css.building);
+    // update the UI
+    utils.css.add(dom.gameStatusBar, css.building);
 
-      // and make a noise
-      if (sounds.inventory.begin) {
-        playSound(sounds.inventory.begin);
-      }
+    // and make a noise
+    if (sounds.inventory.begin) {
+      playSound(sounds.inventory.begin);
+    }
 
-      // callback to update queue UI when the build actually begins
-      queueEvents = game.objects.helicopters[0].updateInventoryQueue(newOrder);
+    // callback to update queue UI when the build actually begins
+    queueEvents = game.objects[TYPES.helicopter][0].updateInventoryQueue(newOrder);
 
-      newOrder.onOrderStart = queueEvents.onOrderStart;
-      newOrder.onOrderComplete = queueEvents.onOrderComplete;
+    newOrder.onOrderStart = queueEvents.onOrderStart;
+    newOrder.onOrderComplete = queueEvents.onOrderComplete;
 
-      // display, if provided (relies on queue array to update order counts)
-      if (pendingNotification) {
-        pendingNotification();
-      }
-
+    // display, if provided (relies on queue array to update order counts)
+    if (pendingNotification) {
+      pendingNotification();
     }
 
     // only start processing if queue length is 1 - i.e., first item just added.
@@ -235,25 +219,52 @@ const Inventory = () => {
       processNextOrder();
     }
 
-    // HACK
-    if (options.isEnemy) {
-      data.building = false;
-    }
-
   }
 
   function animate() {
 
+    let newObject;
+
     if (!data.building || data.frameCount++ % objects.order.data.inventory.frameCount !== 0) return;
+
+    if (data.waiting) return processNextOrder();
 
     if (objects.order.size) {
 
       // start building.
-      createObject(objects.order.typeData, objects.order.options);
+      newObject = game.addObject(objects.order.type, objects.order.options);
+
+      // ignore if this is the stub object case
+      if (!objects.order.options.noInit) {
+
+        // force-append this thing, if it's on-screen right now
+        sprites.updateIsOnScreen(newObject);
+
+        utils.css.add(newObject.dom.o, css.building);
+
+        // and start the "rise" animation
+        window.requestAnimationFrame(() => {
+
+          utils.css.add(newObject.dom.o, css.ordering);
+
+          common.setFrameTimeout(() => {
+            if (!newObject.dom?.o) return;
+            utils.css.remove(newObject.dom.o, css.ordering);
+            utils.css.remove(newObject.dom.o, css.building);
+          }, 2200);
+
+        });
+
+      }
+
 
       // only fire "order start" once, whether a single tank or the first of five infantry.
       if (objects.order.size === objects.order.originalSize && objects.order.onOrderStart) {
         objects.order.onOrderStart();
+      }
+
+      if (!objects.order.options.isEnemy) {
+        objects.lastObject = newObject;
       }
 
       objects.order.size--;
@@ -267,9 +278,13 @@ const Inventory = () => {
 
       // "Construction complete."
 
-      // drop the item that just finished building.
-      if (!objects.order.options.isEnemy && objects.order.onOrderComplete) {
-        objects.order.onOrderComplete();
+      if (!objects.order.options.isEnemy) {
+
+        data.waiting = false;
+
+        // drop the item that just finished building.
+        objects.order?.onOrderComplete();
+
       }
       
       processNextOrder();
@@ -331,22 +346,16 @@ const Inventory = () => {
 
   data = {
     frameCount: 0,
-    // quick type-to-object/constructor array
-    types: {
-      tank: [game.objects.tanks, Tank],
-      van: [game.objects.vans, Van],
-      missileLauncher: [game.objects.missileLaunchers, MissileLauncher],
-      infantry: [game.objects.infantry, Infantry],
-      engineer: [game.objects.engineers, Engineer]
-    },
     building: false,
     queue: [],
     queueCopy: [],
-    canShowNSF: false
+    canShowNSF: false,
+    waiting: false
   };
 
   objects = {
-    order: null
+    order: null,
+    lastObject: null
   };
 
   dom = {
@@ -357,7 +366,6 @@ const Inventory = () => {
     animate,
     data,
     dom,
-    createObject,
     order
   };
 
