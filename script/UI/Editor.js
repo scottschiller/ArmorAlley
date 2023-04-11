5e52f874ac73ec77cb207b1b509943073ffd9ccc
Scott Schiller <spam@schillmania.com>
2023-04-10 18:31:21 -0700

    Editor: Radar "scrollbar" feature, scrub to scroll across the battlefield.

--- script/UI/Editor.js
+++ script/UI/Editor.js
@@ -1,5 +1,8 @@
 import { game } from '../core/Game.js';
+import { common } from '../core/common.js';
+import { worldWidth } from '../core/global.js';
 import { utils } from '../core/utils.js';
+
 const Editor = () => {
 
   let css, data, dom, events, exports;
@@ -24,19 +27,122 @@ const Editor = () => {
   }
 
   css = {
+    active: 'active',
     editMode: 'edit-mode',
   };
 
   data = {
+    mouseDown: false,
+    mouseOffsetX: 0,
+    scrubberX: 0,
+    scrubberWidth: 32,
+    scrubberActive: false
   };
 
   dom = {
     oRadarScrubber: null
   };
 
+  function scaleScrubber() {
+
+    // match the viewport width, relative to the world width.
+    const relativeWidth = (game.objects.view.data.browser.width / worldWidth);
+
+    data.scrubberWidth = relativeWidth * game.objects.view.data.browser.width;
+
+    dom.oRadarScrubber.style.width = `${data.scrubberWidth}px`;
+    
+  }
+
+  function getScrubberX(clientX) {
+
+    const maxOverflow = (game.objects.view.data.browser.fractionWidth / worldWidth);
+
+    // move relative to where the slider was grabbed.
+    clientX -= data.mouseOffsetX;
+
+    let xOffset = (clientX / game.objects.view.data.browser.width) * 1 / game.objects.view.data.screenScale;
+
+    xOffset = Math.min(1 - maxOverflow, Math.max(0 - maxOverflow, xOffset));
+
+    game.objects.view.setLeftScroll((xOffset * worldWidth));
+
+    const scrubberX = ((game.objects.view.data.browser.width) * xOffset);
+
+    // position, centered, 0-100%.
+    return scrubberX;
+    
+  }
+
+  function setLeftScroll(clientX) {
+
+    data.scrubberX = getScrubberX(clientX);
+
+    dom.oRadarScrubber.style.transform = `translate(${data.scrubberX}px, 0px)`;
+
+  }
+
 
   events = {
 
+    mousedown(e) {
+
+      data.mouseDown = true;
+
+      if (e.target === dom.oRadarScrubber) {
+
+        data.scrubberActive = true;
+        utils.css.add(dom.oRadarScrubber, css.active);
+        // move relative to "grab point"
+        data.mouseOffsetX = e.clientX - (data.scrubberX * game.objects.view.data.screenScale);
+        e.preventDefault();
+
+        return;
+
+      }
+
+      makeItemAtMouse(e);
+
+    },
+
+    mousemove(e) {
+
+      if (data.scrubberActive) {
+
+        setLeftScroll(e.clientX);
+
+      } else {
+
+        if (data.mouseDown) {
+          makeItemAtMouse(e);
+        }
+        
+      }
+
+    },
+
+    mouseup(e) {
+
+      if (data.scrubberActive) {
+        utils.css.remove(dom.oRadarScrubber, css.active);
+        data.scrubberActive = false;
+      }
+
+      data.mouseDown = false;
+
+    },
+
+    resize() {
+      
+      scaleScrubber();
+
+      // move to where the view is now at.
+      // TODO: ensure this value stays in sync with the scroll width calculations.
+      data.scrubberX = (game.objects.view.data.battleField.scrollLeft / worldWidth) * game.objects.view.data.browser.width;
+      dom.oRadarScrubber.style.transform = `translate(${data.scrubberX}px, 0px)`;
+      
+
+    }
 
   };
 
