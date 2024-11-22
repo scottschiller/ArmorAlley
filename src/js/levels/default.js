37cb5c714f308bc415bd36b9fab6eaed2a19aef2
Scott Schiller <spam@schillmania.com>
2024-11-22 15:07:37 -0800

    Break `defeatMessages` out into `battle-over` module.

--- src/js/levels/default.js
+++ src/js/levels/default.js
@@ -336,52 +336,14 @@ function getVictoryMessage() {
   return msg + getMedals();
 }
 
-let defeatMessages = [
-  `Avoid picking up engineers; they can not take their equipment on board.`,
-  `Methods to destroy anti-aircraft guns include: two smart missiles in quick succession, tanks, paratroopers or engineers.`,
-  `You can take over enemy anti-aircraft guns with your engineers; this will help to protect you and your convoys from airborne assault.`,
-  `Attack ground units from behind.`,
-  `To practice bombing, stay near your base so you can rearm quickly.`,
-  `It is dangerous to fight over your convoys.`,
-  `Unless the enemy helicopter is near by, take time to carefully aim your weapons at ground units.`,
+  // clipSpeedI
+  iq += Math.floor((o[1] * 20) / MAXVELX);
 
-  // TODO: confirm in original game. Not sure about this one.
-  `Fire doesn't take sides.`,
+  // fundMinI
+  iq += Math.floor(((100 - o[2]) * 4) / 100);
 
-  `Create more than one convoy. This way, if your leading convoy is destroyed, a new convoy will be ready to continue the assault immediately.`,
-
-  // HINT: Pick up an engineer for a partial repair: fuel, ammo and so on.
-  `You can repair your helicopter in the field if you have men on board.`,
-
-  `Land to conserve fuel.`,
-  `When you destroy the enemy, he comes back to the field fully armed and ready for battle. If the enemy is going back for supplies, it may be advantageous to let him spend the time to return to his base and refuel rather than destroy him.`,
-  `Balloons and bunkers can provide a strategic advantage. Use infantry to retake bunkers and launch balloons.`,
-  `This is a strategy game. It isn't easily won with quick reactions and superb dexterity. The key to winning is to use tactics to achieve strategic goals.`,
-  `The enemy helicopter can be destroyed with missiles and bombs.`,
-  `Set goals like destroying an enemy anti-aircraft gun or capturing an enemy bunker.`,
-  `Determine exactly what is destroying your forces.`,
-  `Look for blind spots in automated defenses.`,
-  `Try attacking things from another angle.`,
-  `If you're having problems landing on the pad, simply keep the mouse above the landing pad. When the helicopter is over the pad, lower the mouse.`,
-  `On level one, the enemy does not use his missiles. This allows you destroy the enemy helicopter from a safe distance with your own missiles. By arming your convoys with missile trucks you can keep your convoy in relative safety from the enemy helicopter.`,
-  `Destroy enemy missiles by leading them into other objects or shoot them down with machine guns &amp; missiles.`,
-  `Your helicopter can be moved with great precision and varying speeds. To move slowly, don't move the mouse to the extremes of the screen; instead, lead the helicopter just slightly.`,
-  `Always have at least one helicopter in reserve. As long as you destroy the enemy vans and have a helicopter in reserve you won't be able to lose instantly.`,
-  `Survive! Each helicopter is worth 20 funds. A helicopter with a full load of men is worth 25 funds!`,
-  `Build convoys that can survive both ground assaults and attacks from the enemy helicopter. Defend your convoy. Try different combinations of equipment in your convoys.`,
-  `Distract the enemy from your convoys by convincing the enemy copter to follow you.`,
-  `Use smart missiles to distract the enemy.`,
-  `Use machine guns on men, tanks, and other items.`,
-  `Learn to recognize items on the radar.`,
-  `Capture the end bunkers to cut off enemy production.`,
-  `Timing and teamwork are important in multi-player games.`,
-  `Evasive action for misleading enemy guns includes: changing speeds and directions.`,
-  `Can you utilize the enemy landing pad?`,
-  `Use your paratroopers as missile fodder.`,
-  `Use your bombs in quick succession to prevent targets from performing emergency repairs.`,
-  `Missiles can be used against other missiles.`,
-  `When your helicopter is destroyed, fire weapons to continue viewing the scene.`
-];
+  // convoyLevelI
+  iq += o[4];
 
 function getDefeatMessage() {
   return `<span class="game-over-tip"><span class="inline-emoji">💡</span>${defeatMessages[rndInt(defeatMessages.length)]}</span><a href="${window.location.href}" data-ignore-touch="true" class="game-start large">Try again &nbsp;<span class="inline-emoji emoji-text">🚁</span></a>`;
