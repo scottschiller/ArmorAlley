f67257c82207de95e6c32e168f5d42ddf3d32454
Scott Schiller <spam@schillmania.com>
2024-11-27 16:12:20 -0800

    Break out all network levels into separate modules.

---
+++ src/js/levels/network/rainstorm.js
@@ -0,0 +1,31 @@
+const rainstormLevel = [
+  {
+    t: 'balloon',
+    l: [3584, 4096, 4608]
+  },
+  {
+    t: 'base',
+    l: [192],
+    r: [8000]
+  },
+  {
+    t: 'end-bunker',
+    l: [24],
+    r: [8168]
+  },
+  {
+    t: 'landing-pad',
+    n: [320],
+    r: [7872]
+  },
+  {
+    t: 'palm-tree',
+    s: [4735]
+  },
+  {
+    t: 'tree',
+    s: [1299, 3941, 6991, 7109]
+  }
+];
+
+export { rainstormLevel };
