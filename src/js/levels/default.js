509db974bb8388a25f28a6426258249d44a31d2d
Scott Schiller <spam@schillmania.com>
2024-11-27 16:04:03 -0800

    Split Tutorial into its own module.

--- src/js/levels/default.js
+++ src/js/levels/default.js
@@ -7,6 +7,7 @@ import { scanNodeTypes } from '../UI/Radar.js';
 import { prefsManager, screenScale } from '../aa.js';
 import { utils } from '../core/utils.js';
 import { FACING } from '../elements/Balloon.js';
+import { tutorialLevel } from './tutorial.js';
 
 // Default "world": Tutorial, level 1 or level 9 (roughly)
 
@@ -929,90 +930,6 @@ const n = 'n',
   r = 'r';
 
 originalLevels = {
-  'Tutorial': [
-    {
-      t: 'balloon',
-      l: [3840, 4352, 4608, 4864]
-    },
-    {
-      t: 'barb-wire',
-      s: [918, 5200]
-    },
-    {
-      t: 'base',
-      l: [160],
-      r: [8000]
-    },
-    {
-      t: 'bunker',
-      r: [1024, 4608, 5120, 5632, 6656],
-      l: [1536, 2048, 2560, 3072]
-    },
-    {
-      t: 'checkmark-grass',
-      s: [4120]
-    },
-    {
-      t: 'cloud',
-      s: [512, 3840, 4352, 4608, 4864]
-    },
-    {
-      t: 'end-bunker',
-      l: [8],
-      r: [8144]
-    },
-    {
-      t: 'flower',
-      s: [1850, 7500]
-    },
-    {
-      t: 'grave-cross',
-      s: [4970, 5275]
-    },
-    {
-      t: 'gravestone',
-      s: [2150, 7038, 7160]
-    },
-    {
-      t: 'landing-pad',
-      l: [300, 3806],
-      r: [7800]
-    },
-    {
-      t: 'left-arrow-sign',
-      s: [7700, 8208]
-    },
-    {
-      t: 'palm-tree',
-      s: [
-        860, 1120, 1390, 1565, 2260, 3400, 3490, 4550, 4850, 3964, 4247, 6888,
-        7310
-      ]
-    },
-    {
-      t: 'right-arrow-sign',
-      s: [-48]
-    },
-    {
-      t: 'rock2',
-      s: [1280]
-    },
-    {
-      t: 'super-bunker',
-      r: [3328]
-    },
-    {
-      t: 'tree',
-      s: [
-        660, 2110, 2460, 2700, 4608, 4820, 5110, 4017, 4337, 6736, 6800, 7070,
-        7325
-      ]
-    },
-    {
-      t: 'turret',
-      n: [475, 3456, 4216]
-    }
-  ],
 
   'demo 1': [
     {
@@ -2954,6 +2871,7 @@ originalLevels = {
       s: [1299, 3941, 6991, 7109]
     }
   ]
+  'Tutorial': tutorialLevel,
 };
 
 export {
