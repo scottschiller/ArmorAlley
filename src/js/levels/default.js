e362805628c90738cbd3521934c83cbe36a0d3ea
Scott Schiller <spam@schillmania.com>
2024-12-07 09:11:42 -0800

    Break item stats out into own module.

--- src/js/levels/default.js
+++ src/js/levels/default.js
@@ -30,6 +30,7 @@ import { tankersDemiseLevel } from './network/tankers-demise.js';
 import { windStalkerLevel } from './network/windstalker.js';
 import { sandstormLevel } from './network/sandstorm.js';
 import { rainstormLevel } from './network/rainstorm.js';
+import { itemStats } from './item-stats.js';
 
 // Default "world": Tutorial, level 1 or level 9 (roughly)
 
@@ -319,50 +320,6 @@ const networkParams = [
   [511, 14, 60, h, 10, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
   [511, 14, 60, h, 10, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1]
 ];
-
-const itemStatsStruct = [
-  // based on original
-  'iHits',
-  'iMass',
-  'iCost',
-  'iCreateScore',
-  'iDieScore',
-  'iKillScore',
-  'iBonusScore',
-  'limitI'
-];
-
-// TODO: move into global or something for reference by costs, inventory limits etc.
-const rawItemStats = {
-  [TYPES.helicopter]: [20, 25, 20, 0, -19, 50, 21, 99],
-  [TYPES.smartMissile]: [25, 7, 0, 0, 0, 0, 0, 0],
-  [TYPES.gunfire]: [1, 0, 0, 0, 0, 0, 0, 0],
-  [TYPES.bomb]: [7, 0, 0, 0, 0, 0, 0, 0],
-  [TYPES.shrapnel]: [1, 0, 0, 0, 0, 0, 0, 0],
-  [TYPES.bunker]: [69, 50, 0, 0, -25, -25, 4, 0],
-  [TYPES.superBunker]: [5, 55, 0, 0, -25, -25, 4, 0], // copied from bunker, originally all zeroes.
-  [TYPES.turret]: [31, 75, 0, -3, 0, 50, 3, 0], // iMass = 75 added
-  [TYPES.balloon]: [6, 0, 0, 0, 0, 0, 0, 0],
-  [TYPES.chain]: [18, 1, 0, 0, 0, 0, 0, 0],
-  [TYPES.tank]: [25, 18, 4, -5, -1, 10, 5, 15],
-  [TYPES.missileLauncher]: [8, 20, 3, -2, 0, 20, 2, 6],
-  [TYPES.van]: [12, 30, 2, -15, -5, 15, 15, 10],
-  [TYPES.infantry]: [5, -4, 5, -1, -2, 2, 2, 30],
-  [TYPES.parachuteInfantry]: [5, -4, 0, 0, -2, 2, 2, 0],
-  [TYPES.endBunker]: [0, 0, 0, 0, 1, -1, 20, 0],
-  [TYPES.base]: [0, 99, 0, 0, 0, 0, 0, 0]
-};
-
-const itemStats = {};
-
-// expand arrays into key/value pairs, based on original structure.
-Object.keys(rawItemStats).forEach((key) => {
-  // itemStats[TYPES.helicopter] = { iHits: 20, iMass: 25, ... }
-  let values = {};
-  // iHits = 20, etc.
-  rawItemStats[key].forEach((value, i) => (values[itemStatsStruct[i]] = value));
-  itemStats[key] = values;
-});
 
 function getLevelParams(paramArray, offset) {
   return unpackLevelParams(offset < 0 ? demoParams[0] : paramArray[offset]);
