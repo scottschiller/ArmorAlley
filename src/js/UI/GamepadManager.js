b40ef3a1de2c151726ce4d57191ab641ad625cad
Scott Schiller <spam@schillmania.com>
2025-01-13 16:59:40 -0800

    🎮 Gamepad API support, first prototype: Gamepad Manager + implementation.

---
+++ src/js/UI/GamepadManager.js
@@ -0,0 +1,650 @@
+/**
+ * 🎮 Gamepad API spec
+ * https://www.w3.org/TR/gamepad/
+ *
+ * The gamepad API exposes arrays of digital buttons, and analog axes.
+ * A form of mapping applies, where button[0] may be a d-pad or start button
+ * and axes[0] is typically assigned to an analog joystick.
+ *
+ * Browsers determine whether a controller has a "standard" mapping or not.
+ * The user agent is expected to determine the mapping of the controller.
+ * In the standard case, layout and mapping should be reliable and consistent.
+ *
+ * https://developer.mozilla.org/en-US/docs/Web/API/Gamepad
+ *
+ * Common gamepads e.g., Sony PlayStation 4 "dualshock" should have standard
+ * mappings, and should work reliably across browsers. However, results vary.
+ *
+ * If a standard mapping is not indicated, "here be dragons."
+ *
+ * Gamepads have an ID string which varies by browser, and may include product
+ * and vendor IDs that can be parsed out. This is reminiscent of UA sniffing.
+ *
+ * Firefox and Chrome appear to provide vendor and product IDs, differently-
+ * formatted. Safari may not provide IDs at all, or perhaps omit IDs when
+ * standard mapping applies.
+ *
+ * In testing, an older 8Bitdo NES30Pro (bluetooth) controller does not get a
+ * "standard" mapping in Firefox or Chrome. In addition, Firefox's mapping of
+ * this controller also differs slightly from that of Chrome.
+ *
+ * The NES30Pro has a d-pad, but its values are mapped to a few axes vs. four
+ * buttons. One axis shows nine different values, reflecting the positions of
+ * the d-pad. On a standard mapping, the four buttons' binary values can
+ * represent 9 directions.
+ *
+ * ↖ ↑ ↗
+ * ← · →
+ * ↙ ↓ ↘
+ *
+ * Safari reports a standard mapping for the NES30Pro, but still has the d-pad
+ * as a single-axis with multiple values at axes[9].
+ *
+ * In Firefox, the d-pad "9-value" number is mapped to axes[0] instead of [9],
+ * and the analog joysticks are on axes[1-4] vs. [0-3].
+ *
+ * The intent of this work is to abstract and normalize the common use cases,
+ * between controller updates and the ability to handle special controllers.
+ */
+
+import { gpConfig, gpMap } from './gamepad-config.js';
+import { addControllers } from './gamepad-known-controllers.js';
+import { gamepadFeature, useGamepad } from './gamepad.js';
+
+const GamepadManager = (options = {}) => {
+  // singular, global "gamepad index" (AA uses only one controller at a time.)
+  let gpi;
+
+  const lastState = {};
+  const lastGamepadState = {};
+  const gamepadState = {};
+
+  let changed;
+
+  // id -> { product, vendor } map
+  let parseIdCache = {};
+
+  // middle / "inactive" position in a 9-axis d-pad
+  const OFFSET_CENTER = 4;
+
+  // DRY
+  const left = true,
+    up = true,
+    right = true,
+    down = true;
+
+  let dpadStates = (function () {
+    // axes-to-directions
+    return [
+      { left, up },
+      { up },
+      { right, up },
+      { left },
+      {},
+      { right },
+      { down, left },
+      { down },
+      { down, right }
+    ];
+  })();
+
+  function handleDPad(btnUp, btnDown, btnLeft, btnRight) {
+    let state = {};
+
+    // default: middle row, neutral
+    let offset = OFFSET_CENTER;
+
+    if (btnUp.pressed) {
+      // top row: 0-2
+      offset = 1;
+      state[up] = up;
+    } else if (btnDown.pressed) {
+      // bottom row: 6-8
+      offset = 7;
+      state[down] = down;
+    }
+
+    if (btnLeft.pressed) {
+      offset--;
+      state[left] = left;
+    } else if (btnRight.pressed) {
+      offset++;
+      state[right] = right;
+    }
+
+    state.offset = offset;
+
+    return state;
+  }
+
+  function handleAxisDPad(axis, axisValues) {
+    /**
+     * Special case (8Bitdo NES30 Pro): d-pad state via single axis vs. buttons.
+     *
+     * ↖ ↑ ↗
+     * ← · →
+     * ↙ ↓ ↘
+     */
+    let state = {};
+
+    let axisState = lastState[`gp${gpi}/${axis}`];
+    if (axisState === undefined) return;
+
+    let axisValue = axisState.value;
+
+    // 0-8 for quick "active state" mapping
+    let offset = axisValues.indexOf(parseFloat(axisValue));
+
+    // e.g., { left, up }
+    if (offset !== -1) {
+      state = {
+        offset,
+        ...dpadStates[offset]
+      };
+    } else {
+      // fallback: no match / unknown
+      state = {
+        offset: -1
+      };
+    }
+
+    return state;
+  }
+
+  function checkForChange(id, value) {
+    let state;
+    if (lastState[id].lastValue !== value) {
+      if (!changed) changed = [];
+      changed.push({ id, value });
+    }
+  }
+
+  function setNewState(id, state) {
+    // initial state
+    const { value } = state;
+    if (!lastState[id]) {
+      lastState[id] = {
+        id,
+        lastValue: value,
+        value
+      };
+    }
+    checkForChange(id, value);
+  }
+
+  function applyChange(obj) {
+    const { id, value } = obj;
+    lastState[id].value = value;
+  }
+
+  function updateLastValue(obj) {
+    const { id, value } = obj;
+    lastState[id].lastValue = lastState[id].value;
+  }
+
+  function refreshState() {
+    // apply gamepad values to mapping
+    let gp = navigator.getGamepads()[gpi];
+
+    if (!gp) {
+      // this should not happen. try refreshing `gpi` in this case?
+      console.warn('refreshState(): no gamepad?');
+      return;
+    }
+
+    let parsedID = parseId(gp.id);
+    let vpid = `${parsedID.vendor}/${parsedID.product}`;
+    let gpc = gpConfig[vpid];
+    let map = gpMap[vpid];
+
+    // this should not happen.
+    if (!map) {
+      console.warn('WTF no map?');
+      return;
+    }
+
+    if (!gamepadState.buttons) {
+      gamepadState.buttons = {};
+    }
+
+    if (!lastGamepadState.buttons) {
+      lastGamepadState.buttons = {};
+    }
+
+    // iterate over map, applying lastState
+    Object.entries(map).forEach(([key, value]) => {
+      // 'r1': (value of btn7)
+      lastGamepadState.buttons[value] = gamepadState.buttons[value];
+      gamepadState.buttons[value] = lastState[`gp${gpi}/${key}`]?.value;
+    });
+
+    // ABXY "diamonds" (or similar groups of buttons)
+    if (!gamepadState.abxy) {
+      gamepadState.abxy = [];
+      lastGamepadState.abxy = [];
+      // initial state
+      gpc.abxy?.forEach?.((abxy, i) => {
+        lastGamepadState.abxy[i] = Object.assign({}, gamepadState.abxy[i]);
+        gamepadState.abxy[i] = {
+          left: null,
+          top: null,
+          right: null,
+          bottom: null
+        };
+      });
+    }
+
+    // iterate over each diamond / group
+    gpc.abxy?.forEach?.((o, i) => {
+      lastGamepadState.abxy[i] = Object.assign({}, gamepadState.abxy[i]);
+      let cs = gamepadState.abxy[i];
+      let ls = lastState;
+      cs.left = ls[`gp${gpi}/${o.left}`].value;
+      cs.top = ls[`gp${gpi}/${o.top}`].value;
+      cs.right = ls[`gp${gpi}/${o.right}`].value;
+      cs.bottom = ls[`gp${gpi}/${o.bottom}`].value;
+    });
+
+    // d-pads (most controllers have just one?)
+    if (!gamepadState.dpads) {
+      gamepadState.dpads = [];
+      lastGamepadState.dpads = [];
+      // initial state
+      gpc.dpads?.forEach?.((dpad, i) => {
+        lastGamepadState.dpads[i] = Object.assign({}, gamepadState.dpads[i]);
+        gamepadState.dpads[i] = {
+          up: null,
+          down: null,
+          left: null,
+          right: null
+        };
+      });
+    }
+
+    gpc.dpads?.forEach?.((o, i) => {
+      let cs = gamepadState.dpads[i];
+      let ls = lastState;
+
+      lastGamepadState.dpads[i] = Object.assign({}, cs);
+
+      if (o.axis) {
+        // special-case, single-axis d-pad
+        gamepadState.dpads[i] = handleAxisDPad(o.axis, o.values);
+      } else {
+        // standard 4-button d-pad
+
+        /**
+         * "offset": convenient indicator of active position.
+         * ↖ ↑ ↗
+         * ← · →
+         * ↙ ↓ ↘
+         */
+
+        // default: middle row, neutral
+        let offset = OFFSET_CENTER;
+
+        cs.up = ls[`gp${gpi}/${o[0]}`].value;
+        if (cs.up) {
+          // top row
+          offset = 1;
+        }
+        cs.down = ls[`gp${gpi}/${o[1]}`].value;
+        if (cs.down) {
+          // bottom row
+          offset = 7;
+        }
+        cs.left = ls[`gp${gpi}/${o[2]}`].value;
+        if (cs.left) {
+          offset--;
+        }
+        cs.right = ls[`gp${gpi}/${o[3]}`].value;
+        if (cs.right) {
+          offset++;
+        }
+        cs.offset = offset;
+      }
+    });
+
+    // joysticks
+    if (!gamepadState.joysticks) {
+      gamepadState.joysticks = [];
+      lastGamepadState.joysticks = [];
+      // initial state
+      gpc.joysticks?.forEach?.((joystick, i) => {
+        gamepadState.joysticks[i] = {
+          x: 0,
+          y: 0,
+          button: 0
+        };
+        // same for "last" values.
+        lastGamepadState.joysticks[i] = {
+          x: 0,
+          y: 0,
+          button: 0
+        };
+      });
+    }
+
+    gpc.joysticks?.forEach?.((o, i) => {
+      // DRY
+      let lgs = lastGamepadState;
+      let ls = lastState;
+      let js = gamepadState.joysticks[i];
+
+      // update "last state"
+      lgs.joysticks[i].x = parseFloat(js.x);
+      lgs.joysticks[i].y = parseFloat(js.y);
+      lgs.joysticks[i].button = js.button;
+
+      // new live values
+      let x = parseFloat(ls[`gp${gpi}/${o.axes[0]}`].value);
+      let y = parseFloat(ls[`gp${gpi}/${o.axes[1]}`].value);
+
+      let jp = data.gamepad.config.joystickOptions?.precision;
+
+      if (jp) {
+        x = parseFloat(x.toFixed(jp));
+        y = parseFloat(y.toFixed(jp));
+      }
+
+      let absX = Math.abs(x);
+      let absY = Math.abs(y);
+
+      // account for drift, if set - ignore values <= threshold.
+      if (js.driftOffset) {
+        if (absX <= js.driftOffset[0]) x = 0;
+        if (absY <= js.driftOffset[1]) y = 0;
+      }
+
+      // minimum
+      if (data.gamepad.config.joystickOptions?.zeroPoint) {
+        if (x !== 0 && absX < data.gamepad.config.joystickOptions.zeroPoint)
+          x = 0;
+        if (y !== 0 && absY < data.gamepad.config.joystickOptions.zeroPoint)
+          y = 0;
+      }
+
+      // update
+      js.x = x;
+      js.y = y;
+
+      js.button = ls[`gp${gpi}/${o.button}`].value;
+    });
+  }
+
+  function applyChanges() {
+    changed?.forEach?.(applyChange);
+
+    refreshState();
+
+    options?.onChange?.();
+
+    changed?.forEach?.(updateLastValue);
+
+    changed = null;
+  }
+
+  function update() {
+    // bail if not active
+    if (!gamepadFeature || !useGamepad || !data.enabled) return;
+
+    gpi = undefined;
+
+    for (const pad of navigator.getGamepads()) {
+      if (!pad?.connected) continue;
+
+      // hackish: assign global while iterating through gamepads.
+      // this is sub-par, but a compromise to maintain one "active" gamepad.
+      gpi = pad.index;
+
+      for (const [i, button] of pad.buttons.entries()) {
+        setNewState(`gp${pad.index}/btn${i}`, {
+          value: button.value,
+          pressed: button.pressed
+        });
+      }
+
+      for (const [i, axis] of pad.axes.entries()) {
+        setNewState(`gp${pad.index}/axes${i}`, {
+          value: axis.toFixed(4)
+        });
+      }
+    }
+
+    if (gpi !== undefined) {
+      applyChanges();
+    }
+  }
+
+  function normalizeId(id) {
+    // gamepad ID strings sometimes have extra spacing.
+    // TODO: review and maybe drop.
+    // .replace(/\s+/g, ' ');
+    return id;
+  }
+
+  function parseId(id) {
+    /**
+     * Gamepad shenanigans: Firefox and Chrome may provide vendor & product IDs.
+     * Safari appears to omit them, and marks controllers as "standard"
+     * (known layout / mapping.)
+     *
+     * Chrome includes vendor + product details in parentheses, with other bits.
+     * NES30 Pro example: "8Bitdo NES30 Pro (Vendor: 2dc8 Product: 9001)"
+     * PS4 DualShock example:
+     * "Wireless Controller (STANDARD GAMEPAD Vendor: 054c Product: 09cc)"
+     *
+     * Firefox does not use parentheses, but rather xxxx-xxxx-NAME in ID.
+     * Firefox's vendor + product may be 3 characters, missing a leading zero
+     * unlike Chrome. This was tested with a Sony PS4 DualShock.
+     */
+
+    if (parseIdCache[id]) return parseIdCache[id];
+
+    let vendor, product;
+
+    if (!id.match(/[()]/) && id.indexOf('-') !== -1) {
+      // Firefox "xxxx-xxxx-CONTROLLER NAME" pattern
+      let bits = id.split('-');
+      vendor = bits[0];
+      // ensure vendor + product are 4 characters
+      if (vendor.length === 3) {
+        vendor = `0${vendor}`;
+      }
+      product = bits[1];
+      if (product.length === 3) {
+        product = `0${product}`;
+      }
+    } else {
+      // match text within (parens)
+      let str = id.match(/\(([^)]+)\)/)?.[1] || '';
+
+      // tighten space for split
+      str = str.replace(/\:\s/g, ' ');
+
+      // replace all remaining spaces
+      str = str.replace(/\s/g, ':');
+
+      // for string matching
+      str = str.toLowerCase();
+
+      let bits = str.split(':');
+
+      // vendor:xxxx
+      vendor = bits[bits.indexOf('vendor') + 1];
+
+      // product:xxx
+      product = bits[bits.indexOf('product') + 1];
+    }
+
+    // fallback: Safari does not provide vendor nor product IDs,
+    // in testing 8Bitdo + PS4 controllers.
+    if (!vendor || !product) {
+      vendor = product = 'standard';
+    }
+
+    let result = { vendor, product };
+
+    parseIdCache[id] = result;
+
+    return result;
+  }
+
+  function addGamePad(e) {
+    const gp = navigator.getGamepads()[e.gamepad.index];
+
+    // if "known", connect this ID to the given configuration.
+    let id = normalizeId(gp.id);
+
+    let parsedID = parseId(gp.id);
+
+    // Gamepad API: mapping should be "standard", if recognized.
+    let isStandard = gp.mapping.match(/standard/i);
+
+    if (!gpConfig[id]) {
+      let vp;
+      if (isStandard) {
+        console.log('Gamepad indicates standard mapping');
+      }
+      vp = gpConfig[`${parsedID.vendor}/${parsedID.product}`];
+      if (vp) {
+        console.log('Found local mapping for controller', vp);
+        gpConfig[id] = vp;
+      } else {
+        console.log('No local mapping for controller');
+        if (isStandard) {
+          console.log('No local mapping found - try standard mapping?');
+        }
+      }
+    }
+
+    data.gamepad.config = gpConfig[id];
+  }
+
+  function removeGamePad(e) {
+    const gp = e.gamepad; // navigator.getGamepads()[e.gamepad.index];
+    // TODO: clean up state?
+  }
+
+  function gamepadHandler(e, connected) {
+    const gp = e.gamepad;
+    console.log(
+      `🎮 Gamepad "${gp.id}" (#${gp.index}) ${connected ? 'connected' : 'disconnected'} - ${gp.buttons.length} buttons, ${gp.axes.length} axes.`
+    );
+
+    if (connected) {
+      addGamePad(e);
+    } else {
+      removeGamePad(e);
+    }
+
+    updateGamepadCount();
+
+    // callback
+    options?.onAddOrRemove?.(data.lastKnownGamepadCount);
+  }
+
+  function updateGamepadCount() {
+    // navigator.getGamepads() can return an array with [null, [Gamepad]]-type entries; filter out null-ish ones.
+    data.lastKnownGamepadCount =
+      navigator.getGamepads()?.filter?.((gp) => !!gp)?.length || 0;
+  }
+
+  function rumble(magnitude = 1, duration = 40) {
+    if (!gamepadFeature) return;
+
+    const pad = navigator.getGamepads()[gpi];
+
+    if (!pad) return;
+
+    // https://caniuse.com/?search=vibrationActuator
+
+    // Firefox?
+    if (pad.hapticActuators) {
+      let ha = pad.hapticActuators;
+      // actuators array may exist, but be empty despite "dual-rumble" e.g., on PS4 Dualshock.
+      if (ha.length) {
+        console.log('pad.hapticActuators', ha, magnitude, duration);
+        ha[0].pulse(magnitude, duration);
+      } else {
+        console.log('No haptic actuators found.');
+      }
+    }
+
+    // Chrome, Edge, Safari
+    if (pad.vibrationActuator) {
+      let va = pad.vibrationActuator;
+
+      /**
+       * If effects[] (as in Chrome), take the first; otherwise, use the type.
+       * This may be redundant and perhaps `type` should always be used.
+       */
+      let effect = va.effects?.[0] || va.type;
+
+      if (
+        va.type &&
+        // Safari implements `canPlayEffectType()` - use if defined.
+        (!va.canPlayEffectType || va.canPlayEffectType?.(effect))
+      ) {
+        console.log('gamepad effect', effect, magnitude, duration);
+        va.playEffect(effect, {
+          startDelay: 0,
+          duration,
+          weakMagnitude: 0,
+          strongMagnitude: magnitude
+        });
+      } else {
+        console.log('no vibration actuator effects found.');
+      }
+    }
+  }
+
+  function addEvents() {
+    window.addEventListener(
+      'gamepadconnected',
+      (e) => {
+        gamepadHandler(e, true);
+      },
+      false
+    );
+
+    window.addEventListener(
+      'gamepaddisconnected',
+      (e) => {
+        gamepadHandler(e, false);
+      },
+      false
+    );
+  }
+
+  function init() {
+    if (!gamepadFeature) return;
+
+    // init standard PS4-style and nes30pro mappings
+    addControllers();
+
+    addEvents();
+  }
+
+  let data = {
+    enabled: false,
+    gamepad: {
+      config: null
+    },
+    lastKnownGamepadCount: 0
+  };
+
+  init();
+
+  return {
+    animate: update,
+    enable: () => (data.enabled = true),
+    disable: () => (data.enabled = false),
+    lastState,
+    lastGamepadState,
+    gamepadState,
+    rumble
+  };
+};
+
+export { GamepadManager };
---
+++ src/js/UI/gamepad.js
@@ -0,0 +1,347 @@
+/**
+ * 🎮 Rough implementation based on Gamepad API spec, with some disclaimers.
+ * Gamepad support can vary, per-browser. Here be dragons, etc.
+ * https://www.w3.org/TR/gamepad/
+ */
+
+import { keyboardMonitor } from '../aa.js';
+import { common } from '../core/common.js';
+import { game } from '../core/Game.js';
+import { FPS } from '../core/global.js';
+import { utils } from '../core/utils.js';
+import { GamepadManager } from './GamepadManager.js';
+
+// for dev / testing
+let gamepadFeature = !!window.location.href.match(/gamepad/i);
+
+// TODO: implement in prefs
+let useGamepad = !!gamepadFeature;
+
+// middle / "inactive" position in a 9-axis d-pad
+let OFFSET_CENTER = 4;
+
+let css = {
+  menuActive: 'active',
+  buttonActive: 'active'
+};
+
+let data = {
+  enabled: false,
+  gamepadX: 0,
+  gamepadY: 0,
+  dPadOffset: OFFSET_CENTER,
+  state: {
+    // hackish: external-facing status
+    isFiring: false
+  }
+};
+
+let dom = {
+  controls: null,
+  inventory: null
+};
+
+// joystick offset: helicopter control
+const FLY = 1;
+
+// joystick offset: inventory UI
+const MENU = 0;
+
+function updateAA() {
+  // AA-specific implementation of gamepad
+  if (!gamepadFeature || !useGamepad) return;
+
+  /**
+   * Joysticks: helicopter and inventory controls
+   */
+
+  // previous
+  let lastX = lastGamepadState.joysticks[FLY].x;
+  let lastY = lastGamepadState.joysticks[FLY].y;
+
+  // current
+  let curX = gamepadState.joysticks[FLY].x;
+  let curY = gamepadState.joysticks[FLY].y;
+
+  let startX = game.objects.joystick.data.screenWidth / 2;
+  let startY = game.objects.joystick.data.screenHeight / 2;
+
+  if ((lastX != 0 || lastY != 0) && curX == 0 && curY == 0) {
+    // only stop once?
+    // game.objects.joystick?.end?.();
+  } else {
+    if (curX != 0 || curY != 0) {
+      if (lastX == 0 && lastY == 0) {
+        // "start" moving
+        game.objects.joystick?.start?.({
+          clientX: startX,
+          clientY: startY,
+          hidden: true
+        });
+      } else {
+        // move with velocity?
+        data.gamepadX += curX * 2;
+        data.gamepadY += curY * 2;
+
+        data.gamepadX = Math.min(50, Math.max(-50, data.gamepadX));
+        data.gamepadY = Math.min(50, Math.max(-50, data.gamepadY));
+
+        game.objects.joystick.move({
+          clientX: startX + data.gamepadX,
+          clientY: startY + data.gamepadY
+        });
+      }
+    }
+  }
+
+  /**
+   * Shoulder buttons: weapons + paratrooper controls
+   */
+
+  const buttonsToControls = {
+    r1: {
+      // missile
+      keyCode: keyboardMonitor.keyMap.smartMissile
+    },
+    r2: {
+      // gun
+      keyCode: keyboardMonitor.keyMap.shift,
+      relatedState: 'isFiring'
+    },
+    l1: {
+      // paratrooper
+      keyCode: keyboardMonitor.keyMap.space
+    },
+    l2: {
+      // bomb
+      keyCode: keyboardMonitor.keyMap.ctrl
+    }
+  };
+
+  // did a button change?
+  for (let btn in buttonsToControls) {
+    if (gamepadState.buttons[btn] === lastGamepadState.buttons[btn]) continue;
+    keyboardMonitor[gamepadState.buttons[btn] ? 'keydown' : 'keyup']({
+      keyCode: buttonsToControls[btn].keyCode,
+      fromAATouch: true
+    });
+    // update local state, e.g., `isFiring`, accordingly
+    if (buttonsToControls[btn].relatedState) {
+      data.state[buttonsToControls[btn].relatedState] =
+        !!gamepadState.buttons[btn];
+    }
+  }
+
+  /**
+   * D-pad(s) - we only check for the first one, here.
+   */
+
+  if (gamepadState.dpads?.[0]) {
+    // check for change
+    let { offset } = gamepadState.dpads[0];
+
+    if (offset === OFFSET_CENTER) {
+      // dpad unchanged and inactive - try joystick
+      let { x, y } = gamepadState.joysticks[MENU];
+
+      let row, col, itemsPerRow, div;
+
+      // 3x3
+      itemsPerRow = 3;
+
+      // one cell width or height
+      div = 1 / itemsPerRow;
+
+      // column (L->R)
+      if (x <= -div) {
+        col = 0;
+      } else if (x >= -div && x <= div) {
+        col = 1;
+      } else {
+        col = 2;
+      }
+
+      // row (T->B)
+      if (y <= -div) {
+        row = 0;
+      } else if (y >= -div && y <= div) {
+        row = 1;
+      } else {
+        row = 2;
+      }
+
+      offset = row * itemsPerRow + col;
+    }
+
+    if (offset !== data.dPadOffset) {
+      let items = dom.inventory;
+
+      if (items[data.dPadOffset]) {
+        // deselect
+        items[data.dPadOffset].style.borderColor = '';
+        // ensure the button is no longer activated
+        utils.css.remove(
+          items[data.dPadOffset].querySelector('a'),
+          css.buttonActive
+        );
+      }
+
+      if (items[offset]) {
+        // inactive / active
+        items[offset].style.borderColor =
+          offset === OFFSET_CENTER ? '#666' : '#33ff33';
+      }
+
+      // mark the menu as being active, or not.
+      utils.css.addOrRemove(
+        dom.controls,
+        offset !== OFFSET_CENTER,
+        css.menuActive
+      );
+
+      // update
+      data.dPadOffset = offset;
+    }
+  }
+
+  /**
+   * Joystick and/or ABXY button(s): Flip helicopter, or inventory order
+   */
+
+  let inventoryAction;
+
+  let js = gamepadState.joysticks;
+  let ljs = lastGamepadState.joysticks;
+
+  let menuJSButtonActive =
+    js[MENU].button && js[MENU].button !== ljs[MENU].button;
+
+  let flyJSButtonActive = js[FLY].button && js[FLY].button !== ljs[FLY].button;
+
+  if (flyJSButtonActive) {
+    // flip if D-pad is not engaged - otherwise, dpad + flight joystick button = select
+    if (data.dPadOffset === OFFSET_CENTER) {
+      game.players.local.flip();
+    } else {
+      inventoryAction = true;
+    }
+  } else if (data.dPadOffset !== OFFSET_CENTER && menuJSButtonActive) {
+    // menu joystick button has been pressed, while dpad active.
+    inventoryAction = true;
+  }
+
+  // ABXY "diamond" button group(s)
+  let abxy = gamepadState.abxy[0];
+  let lastABXY = lastGamepadState.abxy[0];
+
+  if (abxy) {
+    if ((abxy.left && !lastABXY.left) || (abxy.bottom && !lastABXY.bottom)) {
+      inventoryAction = true;
+    }
+  }
+
+  if (inventoryAction && data.dPadOffset !== OFFSET_CENTER) {
+    // user has selected something from the inventory UI.
+    inventoryClick(data.dPadOffset);
+  }
+
+  // is an inventory order still active, d-pad + button being held?
+  let inventoryButtonActive =
+    inventoryAction ||
+    (data.dPadOffset !== OFFSET_CENTER &&
+      (abxy.left || abxy.bottom || js[MENU].button || js[FLY].button));
+
+  // was it this previously active?
+  let inventoryButtonWasActive =
+    lastABXY.left || lastABXY.bottom || ljs[MENU].button || ljs[FLY].button;
+
+  if (inventoryButtonWasActive && !inventoryButtonActive) {
+    let target = dom.inventory[data.dPadOffset].querySelector('a');
+    utils.css.remove(target, css.buttonActive);
+  }
+}
+
+function inventoryClick(offset) {
+  let target = dom.inventory[offset]?.querySelector?.('a');
+
+  if (!target) return;
+
+  // some sort of button - inventory, or helicopter controls.
+  let keyMapValue;
+  let keyCode;
+
+  keyMapValue = target.getAttribute('data-keyMap');
+
+  if (!keyMapValue) return;
+
+  // if a comma-delimited list (e.g., smart missile types), split into an array and pick one.
+  if (keyMapValue.indexOf(',') !== -1) {
+    keyMapValue = oneOf(keyMapValue.split(','));
+  }
+
+  keyCode = keyboardMonitor.keyMap[keyMapValue];
+
+  if (!keyCode) return;
+
+  keyboardMonitor.keydown({ keyCode, fromAATouch: true });
+
+  // update UI
+  utils.css.add(target, css.buttonActive);
+
+  // release key, momentarily
+  common.setFrameTimeout(() => {
+    keyboardMonitor.keyup({ keyCode });
+  }, 1 / FPS);
+}
+
+function updateCSS(lastKnownGamepadCount) {
+  utils.css.addOrRemove(
+    document.body,
+    useGamepad && lastKnownGamepadCount,
+    'has-gamepad'
+  );
+
+  if (!lastKnownGamepadCount) {
+    // TODO: reset state only if no controllers connected at all?
+    Object.keys(data.state).forEach((k) => (data.state[k] = false));
+  }
+}
+
+function enable() {
+  data.enabled = true;
+  gamepadManager.enable();
+}
+
+function disable() {
+  data.enabled = false;
+  gamepadManager.disable();
+}
+
+function init() {
+  // TODO: tie into game prefs etc.
+  if (!gamepadFeature || !useGamepad) return;
+
+  enable();
+
+  dom.controls = document.getElementById('mobile-controls');
+  dom.inventory = Array.from(dom.controls.querySelectorAll('.inventory-item'));
+}
+
+const gamepadManager = GamepadManager({
+  onChange: updateAA, // update / animate()-style callback
+  onAddOrRemove: updateCSS
+});
+
+// DRY
+const { lastGamepadState, gamepadState } = gamepadManager;
+
+// "API" (for now)
+const gamepad = {
+  animate: gamepadManager.animate,
+  data,
+  init,
+  state: gamepadState,
+  rumble: gamepadManager.rumble
+};
+
+export { gamepad, gamepadFeature, useGamepad };
