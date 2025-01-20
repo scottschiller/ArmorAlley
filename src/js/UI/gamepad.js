70350a98c15ea78b024646c029c9ef4ad757c883
Scott Schiller <spam@schillmania.com>
2025-01-20 11:16:34 -0800

    Return empty string, derrr.

--- src/js/UI/gamepad.js
+++ src/js/UI/gamepad.js
@@ -500,7 +500,7 @@ function setActive(isActive) {
 }
 
 function getSubmitHTML() {
-  if (!gamepadFeature || !useGamepad) return;
+  if (!gamepadFeature || !useGamepad) return '';
 
   // Best guess at Sony/PlayStation-brand controllers, for button hints
   let isSony;
