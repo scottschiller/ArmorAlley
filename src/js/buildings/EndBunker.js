62a90e6911f320e76a54045a15abd1f79b6ebf6d
Scott Schiller <spam@schillmania.com>
2025-02-14 10:35:16 -0800

    Apply reversal of "lost" fund penalty when recapturing end bunker.

--- src/js/buildings/EndBunker.js
+++ src/js/buildings/EndBunker.js
@@ -477,6 +477,11 @@ const EndBunker = (options = {}) => {
             let isYours = data.isEnemy === game.players.local.data.isEnemy;
             // if funds were negative due to enemy capture, zero them out.
             if (data.funds < 0) {
+              /**
+               * Reverse some or all "lost" fund penalty, since we are now recovering.
+               * Note that `fundsLost` is +ve, this is an intentional subtraction.
+               */
+              data.fundsLost += parseInt(data.funds, 10);
               let msg = isYours
                 ? 'You have recaptured your end bunker. ⛳'
                 : 'The enemy has recaptured their end bunker. ⛳';
