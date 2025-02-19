608c87110e4a84f59a3a0dbb8f63c7aa57774a18
Scott Schiller <spam@schillmania.com>
2025-02-18 19:19:35 -0800

    360K floppy build, null-ish override for `audioSpriteConfig`

---
+++ src/floppy/src-360/js/config/audioSpriteConfig.js
@@ -0,0 +1,7 @@
+// Note: this is a stub replacement for the audio-free 360K floppy build.
+const audioSpriteConfig = {
+  'sprite': {},
+  'spriteFileName': 'aa-spritesheet'
+};
+
+export { audioSpriteConfig };
