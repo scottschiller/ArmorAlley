60057dfa101f2b1a95391384efa9ba6152f07390
Scott Schiller <spam@schillmania.com>
2025-03-21 15:53:44 -0700

    End Bunkner: Instance to "static" methods.

--- src/js/buildings/EndBunker.js
+++ src/js/buildings/EndBunker.js
@@ -27,340 +27,7 @@ let pattern;
 const EndBunker = (options = {}) => {
   let css, dom, domCanvas, data, exports, height, nearby, objects, radarItem;
 
-  function setFiring(state) {
-    if (state && data.energy) {
-      data.firing = state;
-    } else {
-      data.firing = false;
-    }
-  }
 
-  function hit(points, target) {
-    // only tank gunfire counts against end bunkers.
-    if (
-      target &&
-      target.data.type === 'gunfire' &&
-      target.data?.parentType === TYPES.tank
-    ) {
-      data.energy = Math.max(0, data.energy - points);
-      sprites.updateEnergy(exports);
-      onEnergyUpdate();
-    }
-  }
-
-  function fire() {
-    let fireOptions;
-
-    if (
-      !data.firing ||
-      !data.energy ||
-      data.frameCount % (data.fireModulus * (1 / GAME_SPEED_RATIOED)) !== 0
-    )
-      return;
-
-    fireOptions = {
-      parent: exports,
-      parentType: data.type,
-      isEnemy: data.isEnemy,
-      collisionItems: nearby.items,
-      x: data.x + (data.width + 1),
-      y: data.y + data.gunYOffset, // half of height
-      vX: 2,
-      vY: 0,
-      damagePoints: 5
-    };
-
-    game.addObject(TYPES.gunfire, fireOptions);
-
-    // other side
-    fireOptions.x = data.x - 1;
-
-    // and reverse direction
-    fireOptions.vX = -2;
-
-    game.addObject(TYPES.gunfire, fireOptions);
-
-    if (sounds.genericGunFire) {
-      playSound(sounds.genericGunFire, exports);
-    }
-  }
-
-  function captureFunds(target) {
-    if (game.data.battleOver) return;
-
-    let capturedFunds, allFunds, actor;
-
-    // infantry only get to steal so much at a time.
-    // because they're special, engineers may be able to steal all funds. 💰
-    allFunds = target.data.role && gamePrefs.engineers_rob_the_bank;
-
-    let endBunkers = game.objects[TYPES.endBunker];
-
-    // who gets the loot?
-    let beneficiary = endBunkers[target.data.isEnemy ? 1 : 0];
-
-    // 100% or 50% of available funds, if > 0.
-    capturedFunds = parseInt(
-      Math.max(0, allFunds ? data.funds : data.funds >> 1),
-      10
-    );
-
-    // track how much was stolen from local player, before going negative.
-    data.fundsLost += parseInt(data.funds, 10);
-
-    // once captured, now in the hole financially.
-    // this can be reset by recapturing the bunker, OR earning 25 funds.
-    data.funds = -25;
-
-    // engineer + BnB case, vs.
-    if (target.data.role) {
-      actor = gamePrefs.bnb
-        ? target.data.isBeavis
-          ? 'Beavis'
-          : 'Butt-Head'
-        : 'Your engineer';
-    } else {
-      actor = 'Your infantry';
-    }
-
-    const isYourCapture = data.isEnemy !== game.players.local.data.isEnemy;
-
-    if (!tutorialMode) {
-      let msg;
-      if (isYourCapture) {
-        if (!capturedFunds) {
-          game.objects.notifications.add(
-            `🏦 ${actor} found no enemy funds. 🤷`
-          );
-        } else {
-          if (allFunds) {
-            msg = `🏦 💰 ${actor} captured all ${capturedFunds}${
-              capturedFunds > 1 ? ' enemy funds! 🤑' : ' enemy fund. 😒'
-            }`;
-          } else {
-            msg = `🏦 💸 ${capturedFunds} enemy ${
-              capturedFunds > 1 ? ' funds' : ' fund'
-            } captured! 💰`;
-          }
-
-          if (gamePrefs.bnb) {
-            playSound(sounds.bnb.stolenFunds);
-          }
-        }
-        if (msg) {
-          game.objects.view.setAnnouncement(msg);
-          game.objects.notifications.add(msg);
-        }
-      } else {
-        msg = `The enemy captured your funds. 💸 😱<br />Recover your end bunker to reset funds.`;
-        let msgDelay = 8000;
-        game.objects.view.setAnnouncement(msg, msgDelay);
-        game.objects.notifications.add(msg);
-      }
-    }
-
-    // who gets the loot?
-    beneficiary.data.funds += capturedFunds;
-    beneficiary.data.fundsCaptured += capturedFunds;
-
-    game.objects.view.updateFundsUI();
-
-    if (target) {
-      target.die({ silent: true });
-      playSound(sounds.doorClose, exports);
-      if (capturedFunds) {
-        playSound(sounds.cashMoney);
-      }
-    }
-
-    // force update of the local helicopter
-    // TODO: yeah, this is a bit hackish.
-    game.players.local.updateStatusUI({ funds: true });
-  }
-
-  function registerHelicopter(helicopter) {
-    if (!objects.helicopters.includes(helicopter)) {
-      // track all choppers
-      objects.helicopters.push(helicopter);
-
-      // count, and mark the first R/L we get for funds purposes
-      if (helicopter.data.isEnemy) {
-        objects.rightHelicopterCount++;
-        if (!objects.helicopters.right) {
-          objects.helicopters.right = helicopter;
-        }
-      } else {
-        objects.leftHelicopterCount++;
-        if (!objects.helicopters.left) {
-          objects.helicopters.left = helicopter;
-        }
-      }
-    }
-  }
-
-  function distributeFunds() {
-    if (game.objects.editor || game.data.battleOver) return;
-
-    // edge case: tutorial mode, and no enemy chopper present yet
-    if (!objects.helicopters.length) return;
-
-    // TODO: handle multiple players / shared banking / ??? later.
-
-    // 2x for 60 FPS
-    let interval = Math.floor(
-      data.fundsModulus * FPS * (FPS / 30) * GAME_SPEED_RATIOED
-    );
-
-    let helicopter = objects.helicopters[data.isEnemy ? 'right' : 'left'];
-    let chopperX = helicopter.data.x;
-
-    /**
-     * Based on original: add helicopter "distance" every frame,
-     * then some math at an interval to determine if funds are earned.
-     */
-    data.moneyDistL += data.isEnemy ? worldWidth - chopperX : chopperX;
-
-    if (data.frameCount % interval !== 0) return;
-
-    let teamChopperCount = data.isEnemy
-      ? objects.rightHelicopterCount
-      : objects.leftHelicopterCount;
-
-    // based on original: 8192 x 32 ulGameClock ticks
-    let unitL = worldWidth * interval;
-
-    // no funds earned
-    if (data.moneyDistL <= unitL) return;
-
-    let amt =
-      unitL <<
-      Math.min(
-        (Math.max(0, data.funds) / 20 + teamChopperCount) >>
-          (gameType === 'easy' || tutorialMode
-            ? 4
-            : gameType === 'hard'
-              ? 3
-              : 2),
-        4
-      );
-
-    data.moneyDistL -= amt;
-
-    /**
-     * TODO: co-op "joint banking" option.
-     * Until then, only local players contribute funds.
-     * Otherwise, you'd earn double on each human player's side.
-     */
-    if (helicopter.data.isLocal) {
-      data.funds++;
-      data.fundsEarned++;
-      game.objects.view.updateFundsUI();
-      helicopter.updateStatusUI({ funds: true });
-    } else if (helicopter.data.isCPU) {
-      // TODO: does this need any special handling for network games? :X
-      data.funds++;
-      data.fundsEarned++;
-    }
-  }
-
-  function animate() {
-    sprites.moveWithScrollOffset(exports);
-
-    data.frameCount++;
-
-    nearbyTest(nearby, exports);
-
-    fire();
-
-    distributeFunds();
-
-    // note: end bunkers never die, but leaving this in anyway.
-    return data.dead && !dom.o;
-  }
-
-  function updateHealth(attacker) {
-    // notify if just neutralized by tank gunfire
-    onEnergyUpdate();
-    if (data.energy) return;
-
-    if (attacker?.data?.parentType !== TYPES.tank) return;
-
-    // we have a tank, after all
-    if (attacker.data.isEnemy !== game.players.local.data.isEnemy) {
-      game.objects.notifications.addNoRepeat(
-        'The enemy neutralized your end bunker 🚩'
-      );
-    } else {
-      game.objects.notifications.addNoRepeat(
-        "You neutralized the enemy's end bunker ⛳"
-      );
-    }
-  }
-
-  function onEnergyUpdate() {
-    utils.css.addOrRemove(radarItem?.dom?.o, !data.energy, css.neutral);
-  }
-
-  function onNeutralHiddenChange(isVisible) {
-    utils.css.addOrRemove(
-      radarItem?.dom?.o,
-      !data.energy && isVisible,
-      css.neutral
-    );
-  }
-
-  function getSpriteURL() {
-    // image = base + enemy + theme
-    const file = data.isEnemy ? 'end-bunker-enemy' : 'end-bunker';
-    return gamePrefs.weather === 'snow'
-      ? `snow/${file}_snow.png`
-      : `${file}.png`;
-  }
-
-  function applySpriteURL() {
-    if (!domCanvas.img) return;
-    domCanvas.img.src = utils.image.getImageObject(getSpriteURL());
-  }
-
-  function initEndBunker() {
-    if (game.objects.editor) {
-      dom.o = sprites.create({
-        className: css.className
-      });
-    } else {
-      dom.o = {};
-    }
-
-    const src = getSpriteURL();
-
-    const spriteWidth = 82;
-    const spriteHeight = 38;
-
-    const spriteConfig = {
-      src: utils.image.getImageObject(src),
-      source: {
-        x: 0,
-        y: 0,
-        width: spriteWidth,
-        height: spriteHeight
-      },
-      target: {
-        width: spriteWidth / 2,
-        height: spriteHeight / 2,
-        // TODO: figure out cause of offset, and fix.
-        yOffset: 2.5
-      }
-    };
-
-    domCanvas.img = spriteConfig;
-
-    // this will set x + y for domCanvas
-    sprites.moveTo(exports, data.x, data.y);
-
-    radarItem = game.objects.radar.addItem(exports);
-
-    onEnergyUpdate();
-  }
 
   height = 19;
 
@@ -521,6 +188,363 @@ const EndBunker = (options = {}) => {
 
   return exports;
 };
+
+function setFiring(exports, state) {
+  let { data } = exports;
+
+  if (state && data.energy) {
+    data.firing = state;
+  } else {
+    data.firing = false;
+  }
+}
+
+function hit(exports, points, target) {
+  let { data } = exports;
+
+  // only tank gunfire counts against end bunkers.
+  if (
+    target &&
+    target.data.type === 'gunfire' &&
+    target.data?.parentType === TYPES.tank
+  ) {
+    data.energy = Math.max(0, data.energy - points);
+    sprites.updateEnergy(exports);
+    onEnergyUpdate(exports);
+  }
+}
+
+function fire(exports) {
+  let { data, nearby } = exports;
+
+  let fireOptions;
+
+  if (
+    !data.firing ||
+    !data.energy ||
+    data.frameCount % (data.fireModulus * (1 / GAME_SPEED_RATIOED)) !== 0
+  )
+    return;
+
+  fireOptions = {
+    parent: exports,
+    parentType: data.type,
+    isEnemy: data.isEnemy,
+    collisionItems: nearby.items,
+    x: data.x + (data.width + 1),
+    y: data.y + data.gunYOffset, // half of height
+    vX: 2,
+    vY: 0,
+    damagePoints: 5
+  };
+
+  game.addObject(TYPES.gunfire, fireOptions);
+
+  // other side
+  fireOptions.x = data.x - 1;
+
+  // and reverse direction
+  fireOptions.vX = -2;
+
+  game.addObject(TYPES.gunfire, fireOptions);
+
+  if (sounds.genericGunFire) {
+    playSound(sounds.genericGunFire, exports);
+  }
+}
+
+function captureFunds(exports, target) {
+  if (game.data.battleOver) return;
+
+  let { data } = exports;
+
+  let capturedFunds, allFunds, actor;
+
+  // infantry only get to steal so much at a time.
+  // because they're special, engineers may be able to steal all funds. 💰
+  allFunds = target.data.role && gamePrefs.engineers_rob_the_bank;
+
+  let endBunkers = game.objects[TYPES.endBunker];
+
+  // who gets the loot?
+  let beneficiary = endBunkers[target.data.isEnemy ? 1 : 0];
+
+  // 100% or 50% of available funds, if > 0.
+  capturedFunds = parseInt(
+    Math.max(0, allFunds ? data.funds : data.funds >> 1),
+    10
+  );
+
+  // track how much was stolen from local player, before going negative.
+  data.fundsLost += parseInt(data.funds, 10);
+
+  // once captured, now in the hole financially.
+  // this can be reset by recapturing the bunker, OR earning 25 funds.
+  data.funds = -25;
+
+  // engineer + BnB case, vs.
+  if (target.data.role) {
+    actor = gamePrefs.bnb
+      ? target.data.isBeavis
+        ? 'Beavis'
+        : 'Butt-Head'
+      : 'Your engineer';
+  } else {
+    actor = 'Your infantry';
+  }
+
+  const isYourCapture = data.isEnemy !== game.players.local.data.isEnemy;
+
+  if (!tutorialMode) {
+    let msg;
+    if (isYourCapture) {
+      if (!capturedFunds) {
+        game.objects.notifications.add(`🏦 ${actor} found no enemy funds. 🤷`);
+      } else {
+        if (allFunds) {
+          msg = `🏦 💰 ${actor} captured all ${capturedFunds}${
+            capturedFunds > 1 ? ' enemy funds! 🤑' : ' enemy fund. 😒'
+          }`;
+        } else {
+          msg = `🏦 💸 ${capturedFunds} enemy ${
+            capturedFunds > 1 ? ' funds' : ' fund'
+          } captured! 💰`;
+        }
+
+        if (gamePrefs.bnb) {
+          playSound(sounds.bnb.stolenFunds);
+        }
+      }
+      if (msg) {
+        game.objects.view.setAnnouncement(msg);
+        game.objects.notifications.add(msg);
+      }
+    } else {
+      msg = `The enemy captured your funds. 💸 😱<br />Recover your end bunker to reset funds.`;
+      let msgDelay = 8000;
+      game.objects.view.setAnnouncement(msg, msgDelay);
+      game.objects.notifications.add(msg);
+    }
+  }
+
+  // who gets the loot?
+
+  beneficiary.data.funds += capturedFunds;
+  beneficiary.data.fundsCaptured += capturedFunds;
+
+  game.objects.view.updateFundsUI();
+
+  if (target) {
+    target.die({ silent: true });
+    playSound(sounds.doorClose, exports);
+    if (capturedFunds) {
+      playSound(sounds.cashMoney);
+    }
+  }
+
+  // force update of the local helicopter
+  // TODO: yeah, this is a bit hackish.
+  game.players.local.updateStatusUI({ funds: true });
+}
+
+function registerHelicopter(exports, helicopter) {
+  let { objects } = exports;
+
+  if (!objects.helicopters.includes(helicopter)) {
+    // track all choppers
+    objects.helicopters.push(helicopter);
+
+    // count, and mark the first R/L we get for funds purposes
+    if (helicopter.data.isEnemy) {
+      objects.rightHelicopterCount++;
+      if (!objects.helicopters.right) {
+        objects.helicopters.right = helicopter;
+      }
+    } else {
+      objects.leftHelicopterCount++;
+      if (!objects.helicopters.left) {
+        objects.helicopters.left = helicopter;
+      }
+    }
+  }
+}
+
+function distributeFunds(exports) {
+  if (game.objects.editor || game.data.battleOver) return;
+
+  let { data, objects } = exports;
+
+  // edge case: tutorial mode, and no enemy chopper present yet
+  if (!objects.helicopters.length) return;
+
+  // TODO: handle multiple players / shared banking / ??? later.
+
+  // 2x for 60 FPS
+  let interval = Math.floor(
+    data.fundsModulus * FPS * (FPS / 30) * GAME_SPEED_RATIOED
+  );
+
+  let helicopter = objects.helicopters[data.isEnemy ? 'right' : 'left'];
+  let chopperX = helicopter.data.x;
+
+  /**
+   * Based on original: add helicopter "distance" every frame,
+   * then some math at an interval to determine if funds are earned.
+   */
+  data.moneyDistL += data.isEnemy ? worldWidth - chopperX : chopperX;
+
+  if (data.frameCount % interval !== 0) return;
+
+  let teamChopperCount = data.isEnemy
+    ? objects.rightHelicopterCount
+    : objects.leftHelicopterCount;
+
+  // based on original: 8192 x 32 ulGameClock ticks
+  let unitL = worldWidth * interval;
+
+  // practice mode logic: cut the "distance traveled" requirement "in half, doubling(?) the funds earned rate.
+  // unitL = theGame.practiceB && theTP->humanB ? MONEYDIST/2 : MONEYDIST;
+  // AA version: unitL = worldWidth * interval * (practiceMode ? 0.5 : 1)
+
+  // no funds earned
+  if (data.moneyDistL <= unitL) return;
+
+  let amt =
+    unitL <<
+    Math.min(
+      (Math.max(0, data.funds) / 20 + teamChopperCount) >>
+        (gameType === 'easy' || tutorialMode ? 4 : gameType === 'hard' ? 3 : 2),
+      4
+    );
+
+  data.moneyDistL -= amt;
+
+  /**
+   * TODO: co-op "joint banking" option.
+   * Until then, only local players contribute funds.
+   * Otherwise, you'd earn double on each human player's side.
+   */
+  if (helicopter.data.isLocal) {
+    data.funds++;
+    data.fundsEarned++;
+    game.objects.view.updateFundsUI();
+    helicopter.updateStatusUI({ funds: true });
+  } else if (helicopter.data.isCPU) {
+    // TODO: does this need any special handling for network games? :X
+    data.funds++;
+    data.fundsEarned++;
+  }
+}
+
+function animate(exports) {
+  let { data, dom, nearby } = exports;
+
+  sprites.moveWithScrollOffset(exports);
+
+  data.frameCount++;
+
+  nearbyTest(nearby, exports);
+
+  fire(exports);
+
+  distributeFunds(exports);
+
+  // note: end bunkers never die, but leaving this in anyway.
+  return data.dead && !dom.o;
+}
+
+function updateHealth(exports, attacker) {
+  let { data } = exports;
+
+  // notify if just neutralized by tank gunfire
+  onEnergyUpdate(exports);
+
+  if (data.energy) return;
+
+  if (attacker?.data?.parentType !== TYPES.tank) return;
+
+  // we have a tank, after all
+  if (attacker.data.isEnemy !== game.players.local.data.isEnemy) {
+    game.objects.notifications.addNoRepeat(
+      'The enemy neutralized your end bunker 🚩'
+    );
+  } else {
+    game.objects.notifications.addNoRepeat(
+      "You neutralized the enemy's end bunker ⛳"
+    );
+  }
+}
+
+function onEnergyUpdate(exports) {
+  let { css, data, radarItem } = exports;
+
+  utils.css.addOrRemove(radarItem?.dom?.o, !data.energy, css.neutral);
+}
+
+function onNeutralHiddenChange(exports, isVisible) {
+  let { css, data, radarItem } = exports;
+
+  utils.css.addOrRemove(
+    radarItem?.dom?.o,
+    !data.energy && isVisible,
+    css.neutral
+  );
+}
+
+function getSpriteURL(data) {
+  // image = base + enemy + theme
+  const file = data.isEnemy ? 'end-bunker-enemy' : 'end-bunker';
+  return gamePrefs.weather === 'snow' ? `snow/${file}_snow.png` : `${file}.png`;
+}
+
+function applySpriteURL(exports) {
+  let { data, domCanvas } = exports;
+
+  if (!domCanvas.img) return;
+  domCanvas.img.src = utils.image.getImageObject(getSpriteURL(data));
+}
+
+function initEndBunker(exports) {
+  let { css, data, dom, domCanvas } = exports;
+
+  if (game.objects.editor) {
+    dom.o = sprites.create({
+      className: css.className
+    });
+  } else {
+    dom.o = {};
+  }
+
+  const src = getSpriteURL(data);
+
+  const spriteWidth = 82;
+  const spriteHeight = 38;
+
+  const spriteConfig = {
+    src: utils.image.getImageObject(src),
+    source: {
+      x: 0,
+      y: 0,
+      width: spriteWidth,
+      height: spriteHeight
+    },
+    target: {
+      width: spriteWidth / 2,
+      height: spriteHeight / 2,
+      // TODO: figure out cause of offset, and fix.
+      yOffset: 2.5
+    }
+  };
+
+  domCanvas.img = spriteConfig;
+
+  // this will set x + y for domCanvas
+  sprites.moveTo(exports, data.x, data.y);
+
+  exports.radarItem = game.objects.radar.addItem(exports);
+
+  onEnergyUpdate(exports);
+}
 
 EndBunker.radarItemConfig = () => ({
   width: 8,
