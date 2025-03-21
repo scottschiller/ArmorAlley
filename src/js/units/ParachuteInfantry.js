6815e903a0d7d64e532041733cd4bea3aec2fd34
Scott Schiller <spam@schillmania.com>
2025-03-21 14:24:16 -0700

    Parachute Infantry: Instance to "static" methods.

--- src/js/units/ParachuteInfantry.js
+++ src/js/units/ParachuteInfantry.js
@@ -20,255 +20,13 @@ import { net } from '../core/network.js';
 const ParachuteInfantry = (options = {}) => {
   let dom, data, domCanvas, radarItem, exports;
 
-  function openParachute() {
-    if (data.parachuteOpen) return;
 
-    // undo manual assignment from free-fall animation
-    domCanvas.img.source.frameX = 0;
-
-    // update model with open height
-    data.height = 19;
-    data.halfHeight = data.height / 2;
-
-    // and parachute speed, too.
-    data.vY = 0.3 + rng(0.3, data.type);
-
-    // make the noise
-    if (sounds.parachuteOpen) {
-      playSound(sounds.parachuteOpen, exports);
-    }
-
-    data.parachuteOpen = true;
-  }
-
-  function die(dieOptions = {}) {
-    if (data.dead) return;
-
-    domCanvas.img = null;
-
-    if (!dieOptions?.silent) {
-      effects.inertGunfireExplosion({ exports });
-
-      if (gamePrefs.bnb) {
-        if (data.isEnemy) {
-          playSound(sounds.bnb.dvdPrincipalScream, exports);
-        } else {
-          playSound(sounds.bnb.screamShort, exports);
-        }
-      } else {
-        playSound(sounds.scream, exports);
-      }
-
-      common.addGravestone(exports);
-    }
-
-    sprites.removeNodesAndUnlink(exports);
-
-    data.energy = 0;
-
-    data.dead = true;
-
-    radarItem?.die(dieOptions);
-
-    common.onDie(exports, dieOptions);
-  }
-
-  function hit(hitPoints, target) {
-    // special case: helicopter explosion resulting in a paratrooper - make parachute invincible to shrapnel.
-    if (target?.data?.type === 'shrapnel' && data.ignoreShrapnel) {
-      return false;
-    }
-
-    return common.hit(exports, hitPoints, target);
-  }
-
-  function animate() {
-    let randomWind, bgX;
-
-    if (data.dead) return !dom.o;
-
-    // falling?
-
-    sprites.moveTo(
-      exports,
-      data.x + data.vX * GAME_SPEED_RATIOED,
-      data.y + data.vY * GAME_SPEED_RATIOED
-    );
-
-    if (!data.parachuteOpen) {
-      if (data.y >= data.parachuteOpensAtY) {
-        openParachute();
-      } else if (
-        data.frameCount >=
-        data.panicModulus * (1 / GAME_SPEED_RATIOED)
-      ) {
-        // like Tom Petty, free fallin'.
-        // alternate between 0/1
-        data.panicFrame = !data.panicFrame;
-        domCanvas.img.source.frameX = 3 + data.panicFrame;
-        // recycle
-        data.frameCount = 0;
-      }
-    } else {
-      // "range" of rotation
-      data.angle =
-        -data.swing + data.swing * 2 * data.stepFrames[data.stepFrame];
-
-      data.stepFrame += data.stepFrameIncrement;
-
-      if (data.stepFrame >= data.stepFrames.length - 1) {
-        data.stepFrameIncrement *= -1;
-      } else if (data.stepFrame === 0) {
-        data.stepFrameIncrement *= -1;
-      }
-
-      // (potentially) gone with the wind.
-      if (!net.active && data.frameCount % data.windModulus === 0) {
-        // choose a random direction?
-        if (rng(1, data.type) > 0.5) {
-          // -1, 0, 1
-          randomWind = rngInt(3, data.type) - 1;
-
-          data.vX = randomWind * 0.5 * GAME_SPEED_RATIOED;
-
-          if (randomWind === -1) {
-            // moving left
-            bgX = 1;
-          } else if (randomWind === 1) {
-            // moving right
-            bgX = 2;
-          } else {
-            // not moving!
-            bgX = 0;
-          }
-
-          domCanvas.img.source.frameX = bgX;
-          // choose a new wind modulus, too.
-          data.windModulus = 64 + rngInt(64, data.type);
-        } else {
-          // reset wind effect
-
-          data.vX = 0;
-
-          domCanvas.img.source.frameX = 0;
-        }
-      }
-    }
-
-    if (data.parachuteOpen && data.y >= data.maxYParachute) {
-      data.landed = true;
-
-      // touchdown! die "quietly", and transition into new infantry.
-      // in the network case, this will kill the remote.
-      die({ silent: true });
-
-      const params = {
-        x: data.x,
-        isEnemy: data.isEnemy,
-        // exclude from recycle "refund" / reward case
-        unassisted: false,
-        // this is an object "conversion", doesn't count against score.
-        excludeFromScoreCreate: true
-      };
-
-      game.addObject(TYPES.infantry, params);
-    } else if (!data.parachuteOpen) {
-      if (data.y > data.maxYPanic / 2 && !data.didScream) {
-        if (gamePrefs.bnb) {
-          if (data.isEnemy) {
-            playSound(sounds.bnb.dvdPrincipalScream, exports);
-          } else {
-            playSound(sounds.bnb.screamPlusSit, exports, {
-              onplay: (sound) => {
-                // too late if off-screen, parachute open, dead, or landed (in which case, died silently)
-                if (
-                  !data.isOnScreen ||
-                  data.parachuteOpen ||
-                  data.landed ||
-                  data.dead
-                ) {
-                  skipSound(sound);
-                }
-              }
-            });
-          }
-        } else {
-          playSound(sounds.scream, exports);
-        }
-
-        data.didScream = true;
-      }
-
-      if (data.y >= data.maxY) {
-        // hit ground, and no parachute. gravity is a cruel mistress.
-
-        // special case: mark the "occasion."
-        data.didHitGround = true;
-
-        // reposition, first
-        sprites.moveTo(exports, data.x, data.maxY);
-
-        // balloon-on-skin "splat" sound
-        if (sounds.splat) {
-          playSound(sounds.splat, exports);
-        }
-
-        die();
-      }
-    }
-
-    data.frameCount++;
-  }
-
-  function getSpriteURL() {
-    const parts = [];
-
-    // infantry / engineer
-    parts.push('parachute-infantry');
-
-    if (data.isEnemy) parts.push('enemy');
-
-    return `${parts.join('-')}.png`;
-  }
-
-  function initDOM() {
-    dom.o = {};
-    sprites.moveTo(exports);
-  }
-
-  function checkSmartMissileDecoy() {
-    // given the current helicopter, find missiles targeting it and possibly distract them.
-
-    game.objects[TYPES.smartMissile].forEach((missile) =>
-      missile.maybeTargetDecoy(exports)
-    );
-  }
 
   function initParachuteInfantry() {
     initDOM();
 
     radarItem = game.objects.radar.addItem(exports);
 
-    checkSmartMissileDecoy();
-  }
-
-  function makeStepFrames(duration = 1.75, reverse) {
-    // NOTE: duration parameter added, here.
-    duration = FPS * duration * (1 / GAME_SPEED);
-    data.stepFrames = [];
-
-    for (let i = 0; i <= duration; i++) {
-      // 1/x, up to 1
-      data.stepFrames[i] = common.easing.easeInOutSine(i / duration);
-    }
-    if (reverse) {
-      data.stepFrames.reverse();
-    }
-    data.stepFrame = 0;
-    data.stepActive = true;
-  }
-
-  let type = TYPES.parachuteInfantry;
 
   const energy = 5;
 
@@ -311,24 +69,6 @@ const ParachuteInfantry = (options = {}) => {
     },
     options
   );
-
-  // note: duration param.
-  makeStepFrames(1);
-
-  // animation "half-way"
-  data.stepFrame = Math.floor(data.stepFrames.length / 2);
-
-  // reverse animation
-  if (rng(1, data.type) >= 0.5) {
-    data.stepFrameIncrement *= -1;
-  }
-
-  const spriteWidth = 140;
-  const spriteHeight = 40;
-
-  // horizontal sprite arrangement.
-  const frameWidth = spriteWidth / 5;
-  const frameHeight = spriteHeight;
 
   domCanvas = {
     img: {
@@ -396,5 +136,263 @@ const ParachuteInfantry = (options = {}) => {
 
   return exports;
 };
+
+function openParachute(exports) {
+  let { data, domCanvas } = exports;
+
+  if (data.parachuteOpen) return;
+
+  // undo manual assignment from free-fall animation
+  domCanvas.img.source.frameX = 0;
+
+  // update model with open height
+  data.height = 19;
+  data.halfHeight = data.height / 2;
+
+  // and parachute speed, too.
+  data.vY = 0.3 + rng(0.3, data.type);
+
+  // make the noise
+  if (sounds.parachuteOpen) {
+    playSound(sounds.parachuteOpen, exports);
+  }
+
+  data.parachuteOpen = true;
+}
+
+function die(exports, dieOptions = {}) {
+  let { data, domCanvas, radarItem } = exports;
+
+  if (data.dead) return;
+
+  domCanvas.img = null;
+
+  if (!dieOptions?.silent) {
+    effects.inertGunfireExplosion({ exports });
+
+    if (gamePrefs.bnb) {
+      if (data.isEnemy) {
+        playSound(sounds.bnb.dvdPrincipalScream, exports);
+      } else {
+        playSound(sounds.bnb.screamShort, exports);
+      }
+    } else {
+      playSound(sounds.scream, exports);
+    }
+
+    common.addGravestone(exports);
+  }
+
+  sprites.removeNodesAndUnlink(exports);
+
+  data.energy = 0;
+
+  data.dead = true;
+
+  radarItem?.die(dieOptions);
+
+  common.onDie(exports, dieOptions);
+}
+
+function hit(exports, hitPoints, target) {
+  let { data } = exports;
+
+  // special case: helicopter explosion resulting in a paratrooper - make parachute invincible to shrapnel.
+  if (target?.data?.type === 'shrapnel' && data.ignoreShrapnel) {
+    return false;
+  }
+
+  return common.hit(exports, hitPoints, target);
+}
+
+function animate(exports) {
+  let { data, dom, domCanvas } = exports;
+
+  let randomWind, bgX;
+
+  if (data.dead) return !dom.o;
+
+  // falling?
+
+  sprites.moveTo(
+    exports,
+    data.x + data.vX * GAME_SPEED_RATIOED,
+    data.y + data.vY * GAME_SPEED_RATIOED
+  );
+
+  if (!data.parachuteOpen) {
+    if (data.y >= data.parachuteOpensAtY) {
+      openParachute(exports);
+    } else if (
+      data.frameCount >=
+      data.panicModulus * (1 / GAME_SPEED_RATIOED)
+    ) {
+      // like Tom Petty, free fallin'.
+      // alternate between 0/1
+      data.panicFrame = !data.panicFrame;
+      domCanvas.img.source.frameX = 3 + data.panicFrame;
+      // recycle
+      data.frameCount = 0;
+    }
+  } else {
+    // "range" of rotation
+    data.angle = -data.swing + data.swing * 2 * data.stepFrames[data.stepFrame];
+
+    data.stepFrame += data.stepFrameIncrement;
+
+    if (data.stepFrame >= data.stepFrames.length - 1) {
+      data.stepFrameIncrement *= -1;
+    } else if (data.stepFrame === 0) {
+      data.stepFrameIncrement *= -1;
+    }
+
+    // (potentially) gone with the wind.
+    if (!net.active && data.frameCount % data.windModulus === 0) {
+      // choose a random direction?
+      if (rng(1, data.type) > 0.5) {
+        // -1, 0, 1
+        randomWind = rngInt(3, data.type) - 1;
+
+        data.vX = randomWind * 0.5 * GAME_SPEED_RATIOED;
+
+        if (randomWind === -1) {
+          // moving left
+          bgX = 1;
+        } else if (randomWind === 1) {
+          // moving right
+          bgX = 2;
+        } else {
+          // not moving!
+          bgX = 0;
+        }
+
+        domCanvas.img.source.frameX = bgX;
+        // choose a new wind modulus, too.
+        data.windModulus = 64 + rngInt(64, data.type);
+      } else {
+        // reset wind effect
+
+        data.vX = 0;
+
+        domCanvas.img.source.frameX = 0;
+      }
+    }
+  }
+
+  if (data.parachuteOpen && data.y >= data.maxYParachute) {
+    data.landed = true;
+
+    // touchdown! die "quietly", and transition into new infantry.
+    // in the network case, this will kill the remote.
+    die(exports, { silent: true });
+
+    const params = {
+      x: data.x,
+      isEnemy: data.isEnemy,
+      // exclude from recycle "refund" / reward case
+      unassisted: false,
+      // this is an object "conversion", doesn't count against score.
+      excludeFromScoreCreate: true
+    };
+
+    game.addObject(TYPES.infantry, params);
+  } else if (!data.parachuteOpen) {
+    if (data.y > data.maxYPanic / 2 && !data.didScream) {
+      if (gamePrefs.bnb) {
+        if (data.isEnemy) {
+          playSound(sounds.bnb.dvdPrincipalScream, exports);
+        } else {
+          playSound(sounds.bnb.screamPlusSit, exports, {
+            onplay: (sound) => {
+              // too late if off-screen, parachute open, dead, or landed (in which case, died silently)
+              if (
+                !data.isOnScreen ||
+                data.parachuteOpen ||
+                data.landed ||
+                data.dead
+              ) {
+                skipSound(sound);
+              }
+            }
+          });
+        }
+      } else {
+        playSound(sounds.scream, exports);
+      }
+
+      data.didScream = true;
+    }
+
+    if (data.y >= data.maxY) {
+      // hit ground, and no parachute. gravity is a cruel mistress.
+
+      // special case: mark the "occasion."
+      data.didHitGround = true;
+
+      // reposition, first
+      sprites.moveTo(exports, data.x, data.maxY);
+
+      // balloon-on-skin "splat" sound
+      if (sounds.splat) {
+        playSound(sounds.splat, exports);
+      }
+
+      die(exports);
+    }
+  }
+
+  data.frameCount++;
+}
+
+function getSpriteURL(data) {
+  const parts = [];
+
+  // infantry / engineer
+  parts.push('parachute-infantry');
+
+  if (data.isEnemy) parts.push('enemy');
+
+  return `${parts.join('-')}.png`;
+}
+
+function initDOM(exports) {
+  let { dom } = exports;
+  dom.o = {};
+  sprites.moveTo(exports);
+}
+
+function checkSmartMissileDecoy(exports) {
+  // given the current helicopter, find missiles targeting it and possibly distract them.
+
+  game.objects[TYPES.smartMissile].forEach((missile) =>
+    missile.maybeTargetDecoy(exports)
+  );
+}
+
+function initParachuteInfantry(exports) {
+  initDOM(exports);
+
+  exports.radarItem = game.objects.radar.addItem(exports);
+
+  checkSmartMissileDecoy(exports);
+}
+
+function makeStepFrames(exports, duration = 1.75, reverse) {
+  let { data } = exports;
+
+  // NOTE: duration parameter added, here.
+  duration = FPS * duration * (1 / GAME_SPEED);
+  data.stepFrames = [];
+
+  for (let i = 0; i <= duration; i++) {
+    // 1/x, up to 1
+    data.stepFrames[i] = common.easing.easeInOutSine(i / duration);
+  }
+  if (reverse) {
+    data.stepFrames.reverse();
+  }
+  data.stepFrame = 0;
+  data.stepActive = true;
+}
 
 export { ParachuteInfantry };
