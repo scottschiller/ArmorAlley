ac799dda3e35ae15d22c171eb8abb9e777a401df
Scott Schiller <spam@schillmania.com>
2025-03-22 19:22:20 -0700

    Smoke: Instance to "static" methods.

--- src/js/elements/Smoke.js
+++ src/js/elements/Smoke.js
@@ -16,141 +16,6 @@ import { levelFlags } from '../levels/default.js';
 const Smoke = (options = {}) => {
   let dom, domCanvas, data, exports;
 
-  function die() {
-    if (data.dead) return;
-
-    sprites.nullify(dom);
-
-    data.dead = true;
-  }
-
-  function animate() {
-    let scale = null;
-
-    // TODO: implement or drop scaling of smoke sprites in domCanvas.
-    if (data.vX !== null && data.vY !== null) {
-      data.x +=
-        data.vX *
-        (options.fixedSpeed ? 1 : Math.max(0.9, Math.random())) *
-        GAME_SPEED_RATIOED;
-      data.y +=
-        (data.vY * (options.fixedSpeed ? 1 : Math.max(0.9, Math.random())) +
-          data.gravity) *
-        GAME_SPEED_RATIOED;
-
-      if (options.deceleration) {
-        data.vX *= 1 - (1 - options.deceleration) * GAME_SPEED_RATIOED;
-        data.vY *= 1 - (1 - options.deceleration) * GAME_SPEED_RATIOED;
-        if (options.increaseDeceleration !== undefined) {
-          options.deceleration *=
-            1 - (1 - options.increaseDeceleration) * GAME_SPEED_RATIOED;
-        }
-      }
-    }
-
-    // scale applied if also fading out
-    if (data.isFading) {
-      scale = 1 - data.fadeFrame / data.fadeFrames;
-    } else {
-      scale = data.baseScale;
-    }
-
-    if (data.rotation !== undefined) {
-      data.rotation += data.rotationAmount;
-    }
-
-    if (scale) {
-      // data.scale = scale;
-      scale = `scale3d(${[scale, scale, 1].join(', ')})`;
-    }
-
-    if (data.isOnScreen) {
-      sprites.setTransformXY(
-        exports,
-        dom.o,
-        `${data.x}px`,
-        `${data.y}px`,
-        (data.rotation ? `rotate3d(0, 0, 1, ${data.rotation}deg) ` : '') +
-          (scale ? ` ${scale}` : '')
-      );
-    }
-
-    // animate and fade
-    if (data.frameCount % data.spriteFrameModulus === 0) {
-      // first, animate through sprite. then, fade opacity.
-      if (data.spriteFrame < data.spriteFrames) {
-        data.spriteFrame++;
-        domCanvas.img.src = refreshSprite();
-      } else {
-        data.isFading = true;
-      }
-    }
-
-    // if fading, animate every frame.
-    if (data.isFading) {
-      data.fadeFrame += GAME_SPEED_RATIOED;
-
-      if (data.fadeFrame < data.fadeFrames) {
-        domCanvas.img.target.opacity = 1 - data.fadeFrame / data.fadeFrames;
-      }
-
-      if (data.fadeFrame > data.fadeFrames) {
-        // animation finished
-        die();
-      }
-    }
-
-    // smoke particles on radar, why not.
-    if (
-      gamePrefs.radar_enhanced_fx &&
-      !game.objects.radar.data.isJammed &&
-      !data.isStealthSmoke
-    ) {
-      common.domCanvas.draw({
-        data: {
-          type: 'on-radar',
-          x: data.x,
-          y: data.y,
-          // fake it 'til you make it.
-          isOnScreen: true
-        },
-        domCanvas: {
-          width: 1.75,
-          height: 1.75,
-          ctxName: 'radar',
-          draw: (ctx, obj, pos, width, height) => {
-            // special case: don't draw smoke from a cloaked helicopter (i.e., in a cloud.)
-            if (data.parentWasCloaked) return;
-            const left =
-              pos.left(
-                (obj.data.x / worldWidth) *
-                  game.objects.view.data.browser.screenWidth
-              ) - width;
-            const top =
-              (data.y / game.objects.view.data.battleField.height) *
-                game.objects.radar.data.height -
-              height;
-            if (domCanvas.img?.target?.opacity) {
-              ctx.fillStyle = `rgba(153, 153, 153, ${domCanvas.img?.target?.opacity}`;
-            } else {
-              ctx.fillStyle = '#999';
-            }
-            ctx.fillRect(left, top, width, height);
-          }
-        }
-      });
-    }
-
-    data.frameCount++;
-
-    return data.dead && !dom.o;
-  }
-
-  function initSmoke() {
-    // null-ish object, for domCanvas
-    dom.o = {};
-  }
-
   data = common.inheritData(
     {
       type: 'smoke',
@@ -188,10 +53,6 @@ const Smoke = (options = {}) => {
     },
     options
   );
-
-  function refreshSprite() {
-    return utils.image.getImageObject(`smoke_v${data.spriteFrame}.png`);
-  }
 
   domCanvas = {
     img: {
@@ -254,5 +115,148 @@ const Smoke = (options = {}) => {
 
   return exports;
 };
+
+function refreshSprite(data) {
+  return utils.image.getImageObject(`smoke_v${data.spriteFrame}.png`);
+}
+
+function die(exports) {
+  let { data, dom } = exports;
+
+  if (data.dead) return;
+
+  sprites.nullify(dom);
+
+  data.dead = true;
+}
+
+function animate(exports) {
+  let { data, dom, domCanvas, options } = exports;
+
+  let scale = null;
+
+  // TODO: implement or drop scaling of smoke sprites in domCanvas.
+  if (data.vX !== null && data.vY !== null) {
+    data.x +=
+      data.vX *
+      (options.fixedSpeed ? 1 : Math.max(0.9, Math.random())) *
+      GAME_SPEED_RATIOED;
+    data.y +=
+      (data.vY * (options.fixedSpeed ? 1 : Math.max(0.9, Math.random())) +
+        data.gravity) *
+      GAME_SPEED_RATIOED;
+
+    if (options.deceleration) {
+      data.vX *= 1 - (1 - options.deceleration) * GAME_SPEED_RATIOED;
+      data.vY *= 1 - (1 - options.deceleration) * GAME_SPEED_RATIOED;
+      if (options.increaseDeceleration !== undefined) {
+        options.deceleration *=
+          1 - (1 - options.increaseDeceleration) * GAME_SPEED_RATIOED;
+      }
+    }
+  }
+
+  // scale applied if also fading out
+  if (data.isFading) {
+    scale = 1 - data.fadeFrame / data.fadeFrames;
+  } else {
+    scale = data.baseScale;
+  }
+
+  if (data.rotation !== undefined) {
+    data.rotation += data.rotationAmount;
+  }
+
+  if (scale) {
+    // data.scale = scale;
+    scale = `scale3d(${[scale, scale, 1].join(', ')})`;
+  }
+
+  if (data.isOnScreen) {
+    sprites.setTransformXY(
+      exports,
+      dom.o,
+      `${data.x}px`,
+      `${data.y}px`,
+      (data.rotation ? `rotate3d(0, 0, 1, ${data.rotation}deg) ` : '') +
+        (scale ? ` ${scale}` : '')
+    );
+  }
+
+  // animate and fade
+  if (data.frameCount % data.spriteFrameModulus === 0) {
+    // first, animate through sprite. then, fade opacity.
+    if (data.spriteFrame < data.spriteFrames) {
+      data.spriteFrame++;
+      domCanvas.img.src = refreshSprite(data);
+    } else {
+      data.isFading = true;
+    }
+  }
+
+  // if fading, animate every frame.
+  if (data.isFading) {
+    data.fadeFrame += GAME_SPEED_RATIOED;
+
+    if (data.fadeFrame < data.fadeFrames) {
+      domCanvas.img.target.opacity = 1 - data.fadeFrame / data.fadeFrames;
+    }
+
+    if (data.fadeFrame > data.fadeFrames) {
+      // animation finished
+      die(exports);
+    }
+  }
+
+  // smoke particles on radar, why not.
+  if (
+    gamePrefs.radar_enhanced_fx &&
+    !game.objects.radar.data.isJammed &&
+    !data.isStealthSmoke
+  ) {
+    common.domCanvas.draw({
+      data: {
+        type: 'on-radar',
+        x: data.x,
+        y: data.y,
+        // fake it 'til you make it.
+        isOnScreen: true
+      },
+      domCanvas: {
+        width: 1.75,
+        height: 1.75,
+        ctxName: 'radar',
+        draw: (ctx, obj, pos, width, height) => {
+          // special case: don't draw smoke from a cloaked helicopter (i.e., in a cloud.)
+          if (data.parentWasCloaked) return;
+          const left =
+            pos.left(
+              (obj.data.x / worldWidth) *
+                game.objects.view.data.browser.screenWidth
+            ) - width;
+          const top =
+            (data.y / game.objects.view.data.battleField.height) *
+              game.objects.radar.data.height -
+            height;
+          if (domCanvas.img?.target?.opacity) {
+            ctx.fillStyle = `rgba(153, 153, 153, ${domCanvas.img?.target?.opacity}`;
+          } else {
+            ctx.fillStyle = '#999';
+          }
+          ctx.fillRect(left, top, width, height);
+        }
+      }
+    });
+  }
+
+  data.frameCount++;
+
+  return data.dead && !dom.o;
+}
+
+function initSmoke(exports) {
+  // null-ish object, for domCanvas
+  exports.dom.o = {};
+}
 
 export { Smoke };
